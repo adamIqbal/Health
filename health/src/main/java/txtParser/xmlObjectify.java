a24f61bd2bb3847392ae23bf24976b7ebe7110fc
adamIqbal <hamier08@gmail.com>
2015-04-30 21:23:19 +0200

    added xml + fixed dir issue

---
+++ health/src/main/java/txtParser/xmlObjectify.java
@@ -0,0 +1,17 @@
+package txtParser;
+
+import java.util.List;
+
+public class xmlObjectify {
+	/**
+	 * Class shall return string o
+	 * 
+	 * @param e
+	 * @return
+	 */
+
+	public String toString(List<?> e) {
+		return e.toString();
+	}
+
+}
