0017eec6232c248962fda475be29dc69d2278b32
adamIqbal <hamier08@gmail.com>
2015-05-06 11:14:35 +0200

    empty test cases added

---
+++ health/src/test/java/txtParser/TxtParserTest.java
@@ -0,0 +1,14 @@
+package txtParser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TxtParserTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
