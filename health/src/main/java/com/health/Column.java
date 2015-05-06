edf54f82fbb1f7f4275d8bb14076b69d83928d9d
Martijn van Dorp <martijnvandorp@casema.nl>
2015-05-06 13:20:02 +0200

    Updated UML class diagram. Created unit tests for Column class and implemented Column class.

--- health/src/main/java/com/health/Column.java
+++ health/src/main/java/com/health/Column.java
@@ -1,9 +1,62 @@
 package com.health;
 
+import java.util.Objects;
+
+/**
+ * Represents a column of a @see {@link Table}.
+ * 
+ * @author Martijn
+ */
 public class Column {
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+	String name;
+	int index;
+	ValueType type;
+	
+	/**
+	 * Constructs a new column with the given name, index and type.
+	 * 
+	 * @param name the name of the column.
+	 * @param index the index of the column.
+	 * @param type the type of the value of the column.
+	 * @throws NullPointerException if name is null;
+	 * @throws IllegalArgumentException if index is negative.
+	 */
+	public Column(String name, int index, ValueType type) {
+		Objects.requireNonNull(name);
+		
+		if (index < 0) {
+			throw new IllegalArgumentException("Argument index cannot be negative.");
+		}
+		
+		this.name = name;
+		this.index = index;
+		this.type = type;
 	}
 
+	/**
+	 * Gets the name of this column.
+	 * 
+	 * @return the name of this column.
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * Gets the index of this column.
+	 * 
+	 * @return the index of this column.
+	 */
+	public int getIndex() {
+		return this.index;
+	}
+
+	/**
+	 * Gets the type of the value of this column.
+	 * 
+	 * @return the type of the value of this column.
+	 */
+	public ValueType getType() {
+		return this.type;
+	}
 }
