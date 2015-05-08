0a76e0d42f1839aa20c3555f2cbe78766f1f0ab8
adamIqbal <hamier08@gmail.com>
2015-05-08 09:12:49 +0200

    refactor code started

--- health/src/main/java/txtParser/TxtParser.java
+++ health/src/main/java/txtParser/TxtParser.java
@@ -4,19 +4,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class TxtParser {
 
-	public void readStream() {
-		Path path = Paths.get("c:/temp", "data.txt");
+  public void readStream() {
+    Path path = Paths.get("c:/temp", "data.txt");
 
-		try (Stream<String> lines = Files.lines(path)) {
-			
-		//delimit lines, parse into creatObject
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    try (Stream<String> lines = Files.lines(path)) {
+      String str = path.toString();
+      String[] strs = str.split("\\|+");
+      for (String s : strs) {
+        System.out.println("Split..: " + s);
+      }
+
+      Pattern pattern = Pattern.compile("[^|]+");
+      Matcher matcher = pattern.matcher(str);
+      while (matcher.find()) {
+        System.out.println("Pattern:" + matcher.group());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
