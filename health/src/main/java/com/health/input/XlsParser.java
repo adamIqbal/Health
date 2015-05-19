0691cef26ae07d1cc78c3ceb97c24cd84b1b2c27
adamIqbal <hamier08@gmail.com>
2015-05-19 12:01:04 +0200

    parsed xls into sheet

--- health/src/main/java/com/health/input/XlsParser.java
+++ health/src/main/java/com/health/input/XlsParser.java
@@ -1,15 +1,50 @@
 package com.health.input;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 import com.health.Table;
 
+/**
+ * Implements a parser for xls input files.
+ *
+ */
 public class XlsParser implements Parser {
+  /**
+   * Given a path to a xls file and an input descriptor, parses the input file into a {@link Table}.
+   *
+   * @param path
+   *          the path of the input file.
+   * @param config
+   *          the input descriptor.
+   * @return a table representing the parsed input file.
+   * @throws IOException
+   *           if any IO errors occur.
+   * @throws InputException
+   *           if any input errors occur.
+   */
 
   @Override
   public Table parse(String path, InputDescriptor config) throws InputException, IOException {
-    // TODO Auto-generated method stub
-    return null;
+    Objects.requireNonNull(path);
+    Objects.requireNonNull(config);
+
+    Table table = config.buildTable();
+    Workbook wb = new HSSFWorkbook();
+    
+    Sheet sheet = wb.getSheetAt(0);
+    for (Row row : sheet) {
+      for (Cell cell : row) {
+        // Do something here
+      }
+
+    return table;
   }
 
 }
