0a0257d67f5cb2b44607fb14d7ce59d73d2e6f77
adamIqbal <hamier08@gmail.com>
2015-05-21 10:10:01 +0200

    data written to file, converted to string

--- health/src/main/java/com/health/control/ControlModule.java
+++ health/src/main/java/com/health/control/ControlModule.java
@@ -1,127 +1,138 @@
 package com.health.control;
+
+import java.io.IOException;
 import java.util.Arrays;
 
 import com.health.*;
+import com.health.output.Output;
 
 /**
  * 
  *
  */
 public class ControlModule {
-	private Table data;
-	private String script;
+  private Table data;
+  private String script;
 
-	private Table dataset;
+  private Table dataset;
 
-	/**
+  /**
 	 * 
 	 */
-	public ControlModule() {
-		this.data = null;
-		this.script = null;
-		this.dataset = null;
-	}
+  public ControlModule() {
+    this.data = null;
+    this.script = null;
+    this.dataset = null;
+  }
 
-    /**
-     * @param data
-     * @param script
-     */
-    public ControlModule(Table data, String script) {
-    	setData(data);
-    	setScript(script);
-    	this.dataset = null;
+  /**
+   * @param data
+   * @param script
+   */
+  public ControlModule(Table data, String script) {
+    setData(data);
+    setScript(script);
+    this.dataset = null;
+  }
+
+  /**
+   * Start analysis based on script
+   * 
+   * @return
+   */
+  public String startAnalysis() {
+    if (this.data == null) {// || this.script == null) {
+      return null;
     }
 
-	/**
-	 * Start analysis based on script
-     * @return
-     */
-    public boolean startAnalysis() {
-    	if(this.data == null || this.script == null) {
-    		return false;
-    	}
+    // data.script
 
-    	toInputModule();
-    	toOutputModule();
-
-    	return false;
-    }	
-
-    /**
-     * returns result of the script
-     */
-    public void getResults() {
-    	
+    String output = Output.formatTable(data);
+    try {
+      Output.writeTable("output.txt", data);
+    } catch (IOException e) {
+      System.out.println(" data could not be written to file");
     }
 
-    /**
-     * Send data to input module
-     */
-    private void toInputModule() {
-    	//maakt input module aan..
-    	//geef data..
-    	//this.dataset =  gevuld Table object.
+    return output;
+  }
 
-    	return;
-    }
+  /**
+   * returns result of the script
+   */
+  public void getResults() {
 
-    /**
-     * Send data to output module
-     */
-    private void toOutputModule() {
+  }
 
-    }
+  /**
+   * Send data to input module
+   */
+  private void toInputModule() {
+    // maakt input module aan..
+    // geef data..
+    // this.dataset = gevuld Table object.
 
-	/**
-	 * @return
-	 */
-	public Table getData() {
-		return data;
-	}
+    return;
+  }
 
-	/**
-	 * @param data2
-	 */
-	public void setData(Table data2) {
-		this.data = data2;
-	}
+  /**
+   * Send data to output module
+   */
+  private void toOutputModule() {
 
-	/**
-	 * @return
-	 */
-	public String getScript() {
-		return script;
-	}
+  }
 
-	/**
-	 * @param script
-	 */
-	public void setScript(String script) {
-		this.script = script;
-	}
+  /**
+   * @return
+   */
+  public Table getData() {
+    return data;
+  }
 
-	/**
-	 * @return
-	 */
-	public Table getDataset() {
-		return dataset;
-	}
+  /**
+   * @param data2
+   */
+  public void setData(Table data2) {
+    this.data = data2;
+  }
 
-	/**
-	 * @param dataset
-	 */
-	public void setDataset(Table dataset) {
-		this.dataset = dataset;
-	}
+  /**
+   * @return
+   */
+  public String getScript() {
+    return script;
+  }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "ControlModule [data=" + data.toString() + ", script="
-				+ script + ", dataset=" + dataset + "]";
-	}
-    
+  /**
+   * @param script
+   */
+  public void setScript(String script) {
+    this.script = script;
+  }
+
+  /**
+   * @return
+   */
+  public Table getDataset() {
+    return dataset;
+  }
+
+  /**
+   * @param dataset
+   */
+  public void setDataset(Table dataset) {
+    this.dataset = dataset;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "ControlModule [data=" + data.toString() + ", script=" + script + ", dataset=" + dataset
+        + "]";
+  }
+
 }
-
