abd20d4c93b097126aee295e9f11eeb87d8a8ab9
adamIqbal <hamier08@gmail.com>
2015-05-21 10:44:46 +0200

    add fixes to get parsed Data

--- health/src/main/java/com/health/gui/ScriptPanel.java
+++ health/src/main/java/com/health/gui/ScriptPanel.java
@@ -31,90 +31,90 @@ import com.health.input.InputException;
  */
 public class ScriptPanel extends JPanel {
 
-	private static JTextArea scriptArea;
+  private static JTextArea scriptArea;
 
-	/**
-	 * function creates and fill the panel.
-	 */
-	public ScriptPanel() {
+  /**
+   * function creates and fill the panel.
+   */
+  public ScriptPanel() {
 
-		this.setLayout(new BorderLayout());
+    this.setLayout(new BorderLayout());
 
-		JPanel panel = new JPanel(new BorderLayout());
+    JPanel panel = new JPanel(new BorderLayout());
 
-		scriptArea = new JTextArea(2, 1);
-		scriptArea.setBorder(BorderFactory.createLineBorder(Color.black));
+    scriptArea = new JTextArea(2, 1);
+    scriptArea.setBorder(BorderFactory.createLineBorder(Color.black));
 
-		panel.add(scriptArea, BorderLayout.CENTER);
+    panel.add(scriptArea, BorderLayout.CENTER);
 
-		JButton startAnalasisButton = new JButton("Start Analysis");
-		startAnalasisButton.addActionListener(new ListenForStartAnalysis());
+    JButton startAnalasisButton = new JButton("Start Analysis");
+    startAnalasisButton.addActionListener(new ListenForStartAnalysis());
 
-		JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		southPanel.add(startAnalasisButton);
-		panel.add(southPanel, BorderLayout.SOUTH);
+    JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    southPanel.add(startAnalasisButton);
+    panel.add(southPanel, BorderLayout.SOUTH);
 
-		JTextField textAbove = new JTextField("Script: ");
-		textAbove.setEditable(false);
-		textAbove.setDisabledTextColor(Color.black);
+    JTextField textAbove = new JTextField("Script: ");
+    textAbove.setEditable(false);
+    textAbove.setDisabledTextColor(Color.black);
 
-		panel.add(textAbove, BorderLayout.NORTH);
-		panel.setBorder(new EmptyBorder(10, 80, 10, 80));
+    panel.add(textAbove, BorderLayout.NORTH);
+    panel.setBorder(new EmptyBorder(10, 80, 10, 80));
 
-		this.add(panel, BorderLayout.CENTER);
+    this.add(panel, BorderLayout.CENTER);
 
-	}
+  }
 
-	/**
-	 * get the script area component.
-	 *
-	 * @return scriptArea the component of the scriptarea.
-	 */
-	public static JTextArea getScriptArea() {
-		return scriptArea;
-	}
+  /**
+   * get the script area component.
+   *
+   * @return scriptArea the component of the scriptarea.
+   */
+  public static JTextArea getScriptArea() {
+    return scriptArea;
+  }
 
-	/**
-	 * handle start analysis button.
-	 *
-	 * @author daan
-	 *
-	 */
-	private class ListenForStartAnalysis implements ActionListener {
+  /**
+   * handle start analysis button.
+   *
+   * @author daan
+   *
+   */
+  private class ListenForStartAnalysis implements ActionListener {
 
-		/**
-		 * Makes inputData array and calls the control module.
-		 *
-		 * @param e
-		 */
-		public void actionPerformed(final ActionEvent e) {
-			ArrayList<FileListingRow> files = FileListing.getFileListingRows();
-			Table parsedData = new Table(null);
+    /**
+     * Makes inputData array and calls the control module.
+     *
+     * @param e
+     */
+    public void actionPerformed(final ActionEvent e) {
+      ArrayList<FileListingRow> files = FileListing.getFileListingRows();
+      String xmlFormat = "", fileString = "";
+      Table parsedData = null;
 
-			for (int i = 0; i < files.size(); i++) {
-				String xmlFormat = files.get(i).getXmlFormat()
-						.getSelectedItem().toString();
-				String fileString = files.get(i).getFileString();
-				xmlFormat = GUImain.PATHTOXMLFORMATS + xmlFormat + ".xml";
+      for (int i = 0; i < files.size(); i++) {
+        xmlFormat = files.get(i).getXmlFormat().getSelectedItem().toString();
+        fileString = files.get(i).getFileString();
+        xmlFormat = GUImain.PATHTOXMLFORMATS + xmlFormat + ".xml";
 
-				System.out.println(xmlFormat + " " + fileString);
+        System.out.println(xmlFormat + " " + fileString);
+      }
 
-				try {
-          parsedData = Input.readTable(fileString,xmlFormat);
-        } catch (IOException | ParserConfigurationException | SAXException | InputException e1) {
-          System.out.println("Error: Something went wrong parsing the config and data!");
-          e1.printStackTrace();
-        }
-			}
+      try {
+        parsedData = Input.readTable(fileString, xmlFormat);
+      } catch (IOException | ParserConfigurationException | SAXException | InputException e1) {
+        System.out.println("Error: Something went wrong parsing the config and data!");
+        e1.printStackTrace();
+      }
 
-			String script = ScriptPanel.getScriptArea().getText();
+      String script = ScriptPanel.getScriptArea().getText();
 
-			ControlModule control = new ControlModule(parsedData, script);
-			String output = control.startAnalysis();
-			
-			OutputDataPanel.displayData(output);
-			
-		}
+      ControlModule control = new ControlModule(parsedData, script);
+      String output = control.startAnalysis();
 
-	}
+      OutputDataPanel.displayData(output);
+
+    }
+
+  }
 }
