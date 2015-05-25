bfe0af49f05e4e5a9cdef99e08d9b5112263abef
Saffie <bjorn.vdlaan@gmail.com>
2015-05-25 14:36:44 +0200

    added delimiter edit panel

--- health/src/main/java/com/health/gui/XmlWizard.java
+++ health/src/main/java/com/health/gui/XmlWizard.java
@@ -3,6 +3,10 @@ package com.health.gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -10,42 +14,147 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
-import javax.swing.*;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.SoftBevelBorder;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
-public class XmlWizard extends JFrame {
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class XmlWizard extends JFrame implements ActionListener {
+	XmlFilePanel filePanel;
+	XmlDelimiterEditPanel delimPanel;
+	
 	public XmlWizard(String path) {
 		super();
-		this.setLayout(new BorderLayout());
 		this.setSize(500, 500);
+				
+		filePanel = new XmlFilePanel(path);
+		delimPanel = new XmlDelimiterEditPanel();
 		
-		this.add(new XmlFilePanel(path), BorderLayout.WEST);
-		
+		this.getContentPane().add(filePanel);
+		filePanel.addActionListenerToNewFileButton(this);
+		filePanel.addActionListenerToSelectFileButton(this);
+			
 		this.setTitle("XML Editor");
 		this.setVisible(true);
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton source = (JButton) e.getSource();
+		if(source.equals(filePanel.getNewFileButton())) {
+			this.getContentPane().remove(filePanel);
+			this.setContentPane(delimPanel);
+			this.repaint();
+			this.revalidate();
+		}
+		else if(source.equals(filePanel.getSelectFileButton())) {
+			delimPanel.setSelectedXML(filePanel.getSelectedFile());
+			this.getContentPane().remove(filePanel);
+			this.setContentPane(delimPanel);
+			this.repaint();
+			this.revalidate();			
+		}
+	}
 }
 
+/**
+ * Screen where the user can either select a xml file to edit, or create a new xml file
+ * @author Bjorn
+ *
+ */
 class XmlFilePanel extends JPanel {	
+	private JButton newFileButton;
+	private JButton selectFileButton;
+	private FileList fileList;
+
 	public XmlFilePanel(String path) {
 		super();
+		this.setLayout(new BorderLayout());
+		
+		//add list model
 		DefaultListModel<Path> listModel = new DefaultListModel<Path>();
-		this.add(new FileList(path, listModel));
+		fileList = new FileList(path, listModel);
+		this.add(fileList, BorderLayout.CENTER);
+		
+		//add buttons
+		JPanel buttonPanel = new JPanel();
+		newFileButton = new JButton("Create a new file");
+		selectFileButton = new JButton("Edit selected file");
+		buttonPanel.add(newFileButton);
+		buttonPanel.add(selectFileButton);
+		this.add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	public JButton getNewFileButton() {
+		return newFileButton;
+	}
+	
+	public void setNewFileButton(JButton newButton) {
+		this.newFileButton = newButton;
+	}
+
+	public JButton getSelectFileButton() {
+		return selectFileButton;
+	}
+	
+	public void setSelectFileButton(JButton selectButton) {
+		this.selectFileButton = selectButton;
+	}
+	
+	public void addActionListenerToNewFileButton(ActionListener al) {
+		newFileButton.addActionListener(al);
+	}
+	
+	public void addActionListenerToSelectFileButton(ActionListener al) {
+		selectFileButton.addActionListener(al);
+	}
+	
+	public Path getSelectedFile() {
+		return this.fileList.getSelectedValue();
 	}
 }
 
-class XmlEditPanel extends JPanel {
-	public XmlEditPanel() {
+class XmlDelimiterEditPanel extends JPanel {
+	private Path selectedXML;
+	
+	public XmlDelimiterEditPanel() {
 		super();
+		this.setLayout(new GridLayout(0,2));
+		JLabel startDelimLabel = new JLabel("Start Delimiter");
+		JLabel endDelimLabel = new JLabel("End Delimiter");
+		JLabel delimiterLabel = new JLabel("Delimiter");
+		
+		JTextField startDelimField = new JTextField();
+		JTextField endDelimField = new JTextField();
+		JTextField delimiterField = new JTextField();
+		
+		this.add(startDelimLabel);
+		this.add(startDelimField);
+		this.add(delimiterLabel);
+		this.add(delimiterField);
+		this.add(endDelimLabel);
+		this.add(endDelimField);
 	}
-}
 
-class XmlButtonPanel extends JPanel {
-	public XmlButtonPanel() {
-		super();
+	public Path getSelectedXML() {
+		return selectedXML;
 	}
+
+	public void setSelectedXML(Path selectedXML) {
+		this.selectedXML = selectedXML;
+	}
+	
 }
 
 class FileList extends JList<Path> {
@@ -67,12 +176,6 @@ class FileList extends JList<Path> {
 			}
 		} catch (IOException e) {
 			//Directory not found, add no elements
-		}
-		
-		//Just dummy Path objects
-		for(int i = 0; i < 10; i++) {
-			listModel.addElement(Paths.get("path", "to", "files", Integer.toString(i)));
-		}
-		
+		}		
 	}
 }
