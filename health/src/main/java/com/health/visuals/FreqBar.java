30c997964b3bdde46f22103c305972f07c9336b2
LizzyScholten <lizzyxmissxsportyx@hotmail.com>
2015-05-29 17:57:41 +0200

    Checkstyle fixes

--- health/src/main/java/com/health/visuals/FreqBar.java
+++ health/src/main/java/com/health/visuals/FreqBar.java
@@ -21,20 +21,32 @@ import java.io.IOException;
 import java.net.URL;
 
 
+/**
+ * Creates Bar charts.
+ * @author lizzy
+ *
+ */
+public final class FreqBar {
 
-public class FreqBar {
-	
-	public static URL makeBarChart(double[] arIn, String title) throws IOException{
-		
+	private FreqBar() { }
+
+	/**
+	 * Possible preprocessing of info.
+	 * @param arIn			the data
+	 * @param title			title of chart
+	 * @return 				the url of the image
+	 * @throws IOException	if input/output exception of some sort has occurred
+	 */
+	public static URL makeBarChart(final double[] arIn, final String title) throws IOException {
 		double[][] arOut = new double[arIn.length][2];
-		// For checking if event is already 
+		// For checking if event is already
 		boolean check = false;
-		
+
 		int count = 0;
-		for(int i=0; i<arIn.length; i++){ 
+		for (int i = 0; i < arIn.length; i++) {
 			// Check if value is already present in arOut
-			for(int k=0; k<arIn.length; k++){
-				if(arOut[k][0] == arIn[i]){
+			for (int k = 0; k < arIn.length; k++) {
+				if (arOut[k][0] == arIn[i]) {
 					int ind = k;
 					double val = arOut[ind][1];
 					val = val + 1;
@@ -44,31 +56,38 @@ public class FreqBar {
 				}
 			}
 			// Else put it in arOut
-			if(check == false){
+			if (!check) {
 				arOut[count][0] = arIn[i];
 				arOut[count][1] = 1;
 				count = count + 1;
 			}
-			check = false;	
+			check = false;
 		}
-		
+
 		URL url = makeBarChart(arOut, title);
 		return url;
 	}
-	
-	public static URL makeBarChart(double[][] arIn, String title) throws IOException{
+
+	/**
+	 * Creates url for Bar Chart.
+	 * @param arIn			the data
+	 * @param title			title of chart
+	 * @return 				the url of the image
+	 * @throws IOException	if input/output exception of some sort has occurred
+	 */
+	public static URL makeBarChart(final double[][] arIn, final String title) throws IOException {
 		// Defining data
 		double[] dataAr = new double[arIn.length];
 		// Set count to 0
 		int count = 0;
 		// Copy second column of input array into dataArray
-		for(int i=0; i<arIn.length; i++){
+		for (int i = 0; i < arIn.length; i++) {
 			dataAr[count] = arIn[i][1];
 			count = count + 1;
-		}	
+		}
 		// Get maximum value of dataArray
 		double maxVal = dataAr[0];
-		for(int m = 0; m < dataAr.length; m++) {
+		for (int m = 0; m < dataAr.length; m++) {
 			if (dataAr[m] > maxVal) {
 				maxVal = dataAr[m];
 			}
@@ -77,7 +96,7 @@ public class FreqBar {
 		String[] labelsAr = new String[count];
 		count = 0;
 		// Create labels
-		for(int k=0; k<arIn.length; k++){
+		for (int k = 0; k < arIn.length; k++) {
 			labelsAr[count] = String.valueOf(arIn[k][0]);
 			count = count + 1;
 		}
@@ -86,50 +105,49 @@ public class FreqBar {
 		ArrayList<String> newList = new ArrayList<String>();
 		// Delete values from original label array that are useless and that are not going to be used as labels
 		// Do this by copying useful ones in new ArrayList
-		for(int n=0; n<list.size() ;n++){
-			if(!list.get(n).equals("0.0") && !list.get(n).equals(null)){
+		for (int n = 0; n < list.size(); n++) {
+			if (!list.get(n).equals("0.0") && !list.get(n).equals(null)) {
 				newList.add(list.get(n));
-			}	
+			}
 		}
-		
+
 		// Transform labels list to array
 		String[] labelsUse = newList.toArray(new String[newList.size()]);
-		
+
 		// Add data to the chart
-		final double MAX_EVENT = maxVal + 1; 
-		Data data = DataUtil.scaleWithinRange(0,  MAX_EVENT, dataAr); 
+		final double maxEvent = maxVal + 1;
+		Data data = DataUtil.scaleWithinRange(0,  maxEvent, dataAr);
 		BarChartPlot dat = Plots.newBarChartPlot(data, RED, "Data");
 		BarChart chart = GCharts.newBarChart(dat);
-		
+
 		//Defining axis (labels etc)
-		AxisStyle axisStyle = AxisStyle.newAxisStyle(BLACK, 13, AxisTextAlignment.CENTER);		
+		AxisStyle axisStyle = AxisStyle.newAxisStyle(BLACK, 13, AxisTextAlignment.CENTER);
 		AxisLabels event = AxisLabelsFactory.newAxisLabels("Event", 50.0);
 		event.setAxisStyle(axisStyle);
 		AxisLabels events = AxisLabelsFactory.newAxisLabels(Arrays.asList(labelsUse));
 		events.setAxisStyle(axisStyle);
 		AxisLabels frequency = AxisLabelsFactory.newAxisLabels("Frequency", 50.0);
 		frequency.setAxisStyle(axisStyle);
-		AxisLabels freqCount = AxisLabelsFactory.newNumericRangeAxisLabels(0, MAX_EVENT);
+		AxisLabels freqCount = AxisLabelsFactory.newNumericRangeAxisLabels(0, maxEvent);
 		freqCount.setAxisStyle(axisStyle);
-		
+
 		// Add the axis info to the chart.
         chart.addYAxisLabels(freqCount);
         chart.addYAxisLabels(frequency);
         chart.addXAxisLabels(events);
         chart.addXAxisLabels(event);
         chart.setHorizontal(false);
-        
+
         //size should be calculate based on frequencies and amount of labels
         chart.setSize(650, 450);
         chart.setSpaceBetweenGroupsOfBars(20);
-		
+
         // Set title and background
         chart.setTitle(title, BLACK, 17);
         chart.setBackgroundFill(Fills.newSolidFill(LIGHTGREY));
-        
         // Create url of the chart
         URL url = new URL(chart.toURLString());
-        
-        return(url);
+
+        return (url);
 	}
 }
--- health/src/test/java/com/health/visuals/FreqBarTest.java
+++ health/src/test/java/com/health/visuals/FreqBarTest.java
@@ -13,28 +13,42 @@ import javax.swing.JLabel;
 
 import org.junit.Test;
 
-import com.health.visuals.FreqBar;
-
+/**
+ *  Tests the FreqBar class.
+ */
 public class FreqBarTest {
 
-	
+	/**
+	 * Tests FreqBar class with single array.
+	 * @exception IOException if input/output exception of some sort has occurred
+	 */
 	@Test
-	public void test() throws IOException {
-		double[] ar = {1,2,4,3,5,6,3,2,4,3,4,4};
+	public final void test() throws IOException {
+		double[] ar = {1, 2, 4, 3, 5, 6, 3, 2, 4, 3, 4, 4};
 		String title = "Test1";
 		URL url = FreqBar.makeBarChart(ar, title);
 		showChart(url, title);
 	}
-	
+
+	/**
+	 * Tests FreqBar class with 2d array.
+	 * @throws IOException if input/output exception of some sort has occurred
+	 */
 	@Test
-	public void test2() throws IOException {
-		double[][] ar = {{1,1},{2,2},{4,4},{3,3},{5,1},{6,3}};
+	public final void test2() throws IOException {
+		double[][] ar = {{1, 1}, {2, 2}, {4, 4}, {3, 3}, {5, 1}, {6, 3}};
 		String title = "Test2";
 		URL url = FreqBar.makeBarChart(ar, title);
 		showChart(url, title);
 	}
 
-	private static void showChart(URL url, String title) throws IOException {
+	/**
+	 * Creates chart and saves it as file.
+	 * @exception IOException if input/output exception of some sort has occurred
+	 * @param url	an URL indicating the location of the image
+	 * @param title	the name of the chart
+	 */
+	private static void showChart(final URL url, final String title) throws IOException {
 		BufferedImage image = ImageIO.read(url);
 		String t = title + ".jpeg";
 		File file = new File(t);
@@ -44,8 +58,7 @@ public class FreqBarTest {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    f.getContentPane().add(label);
 	    f.pack();
-	    f.setLocation(200,200);
+	    f.setLocation(200, 200);
 	    f.setVisible(true);
-	}	
-	
+	}
 }
