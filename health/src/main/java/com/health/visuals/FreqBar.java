adeb3e525ee01b43bd3747c4104986e1afe64d49
Saffie <bjorn.vdlaan@gmail.com>
2015-06-01 15:18:46 +0200

    FreqBar now checks whether a frequency column is defined

--- health/src/main/java/com/health/visuals/FreqBar.java
+++ health/src/main/java/com/health/visuals/FreqBar.java
@@ -29,20 +29,62 @@ public class FreqBar {
      *            Column to display frequency of
      */
     public static void frequencyBar(final Table table, final String column) {
-        Map<String, Integer> freqMap = createFrequencyMap(table, column);
-        makeBarChart(freqMap);
+        //Check if the Table contains a frequency column
+        Column freqColumn = null;
+        for(Column c : table.getColumns()) {
+            if(c.getIsFrequencyColumn() == true) {
+                freqColumn = c;
+            }
+        }
+        
+        if(freqColumn != null) {
+            Map<String, Integer> freqMap = formatFrequencyMap(table, freqColumn.getName(), column);
+            makeBarChart(freqMap);
+        }
+        //Create frequency map based on column
+        else {
+            Map<String, Integer> freqMap = createFrequencyMap(table, column);
+            makeBarChart(freqMap);
+        }
     }
     
-    private static Map<String, Integer> createFrequencyMap(Table table, String column) {
-        String columnName = column;
+    /**
+     * Creates a frequency map from the input Table to serve as input for makeBarChart.
+     * @param table Table to use
+     * @param freqColumn the frequency column
+     * @param column the column
+     * @return frequency map
+     */
+    private static Map<String, Integer> formatFrequencyMap(final Table table, final String freqColumn, final String column) {        
+        // Create map to save frequencies
+        Map<String, Integer> freqMap = new HashMap<String, Integer>();
         
+        for(Chunk c : table) {
+            for(Record r : c) {
+                String value = r.getValue(column).toString();
+                int frequency = (int) r.getValue(freqColumn);
+                freqMap.put(value, frequency);
+            }
+        }
+        
+        return freqMap;
+    }
+    
+    /**
+     * Counts the occurences of each value of column and creates a frequency map.
+     * Used when no the table contains no frequency column
+     * @param table Table to use
+     * @param column Column to count
+     * @return frequency map
+     */
+    private static Map<String, Integer> createFrequencyMap(final Table table, final String column) {        
         // Create map to save frequencies
         Map<String, Integer> freqMap = new HashMap<String, Integer>();
 
         for (Chunk c : table) {
             for (Record r : c) {
                 // Get value of record
-                String value = r.getValue(columnName).toString();
+                String value = r.getValue(column).toString();
                 if (!freqMap.containsKey(value)) {
                     freqMap.put(value, 1);
                 } else {
