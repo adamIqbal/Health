247ea5d4d01df421211419d9a0d65e255e70b806
Saffie <bjorn.vdlaan@gmail.com>
2015-06-02 12:06:44 +0200

    Created Conversion class to implement conversion operations

---
+++ health/src/main/java/com/health/operations/Conversion.java
@@ -0,0 +1,26 @@
+package com.health.operations;
+
+import com.health.Table;
+
+/**
+ * Class containing conversion operations.
+ * @author Bjorn van der Laan
+ *
+ */
+public final class Conversion {
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private Conversion() {
+        super();
+    }
+    
+    /**
+     * Applies an conversion operation on Table.
+     * @param table Table to apply operation on
+     * @return modified Table after operation
+     */
+    public static Table conversion(Table table) {
+        return null;
+    }
+}
