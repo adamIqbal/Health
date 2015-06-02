f66e995f228fe886075fb21650955927ada468fa
daanvermunt <daanvermunt@gmail.com>
2015-06-02 10:52:20 +0200

    added time chunking and contraint enums

---
+++ health/src/main/java/com/health/Constraints.java
@@ -0,0 +1,25 @@
+package com.health;
+
+public enum Constraints {
+
+	/**
+	 * Greater than
+	 */
+	GreaterThan,
+	/**
+	 * Samller than
+	 */
+	SmallerThan,
+	/**
+	 * Equal to
+	 */
+	EqualTo,
+	/**
+	 * Greater than or equal
+	 */
+	GreaterOrEqual,
+	/**
+	 * Smaller than or equal
+	 */
+	SmallerOrEqual
+}
