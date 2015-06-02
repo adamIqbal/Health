7ce25febff81695178a6fdde4d4940e7b8039bff
Saffie <bjorn.vdlaan@gmail.com>
2015-06-02 11:16:39 +0200

    updated BoxPlot. Nicer plot when only plotting 1 category

--- health/src/main/java/com/health/visuals/BoxPlot.java
+++ health/src/main/java/com/health/visuals/BoxPlot.java
@@ -1,68 +1,82 @@
 package com.health.visuals;
 
+import java.awt.Dimension;
 import java.awt.Font;
-import java.io.IOException;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import javax.swing.JFrame;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.xml.sax.SAXException;
-
-import com.health.Chunk;
-import com.health.Column;
-import com.health.Record;
-import com.health.Table;
-import com.health.input.Input;
-import com.health.input.InputException;
 
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.entity.CategoryItemEntity;
+import org.jfree.chart.entity.EntityCollection;
 import org.jfree.chart.labels.BoxAndWhiskerToolTipGenerator;
+import org.jfree.chart.labels.CategoryToolTipGenerator;
 import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.Outlier;
+import org.jfree.chart.renderer.OutlierList;
+import org.jfree.chart.renderer.OutlierListCollection;
 import org.jfree.chart.renderer.category.BoxAndWhiskerRenderer;
+import org.jfree.chart.renderer.category.CategoryItemRendererState;
+import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.statistics.BoxAndWhiskerCategoryDataset;
 import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RectangleEdge;
+
+import com.health.Chunk;
+import com.health.Record;
 
 public class BoxPlot {
-    
+
     public static void main(final String[] args) {
 
         boxPlot();
 
     }
 
- private static BoxAndWhiskerCategoryDataset createSampleDataset() {
-        
-        final int seriesCount = 3;
-        final int categoryCount = 4;
+    private static BoxAndWhiskerCategoryDataset createSampleDataset() {
+        final int seriesCount = 1;
+        final int categoryCount = 1;
         final int entityCount = 22;
-        
-        final DefaultBoxAndWhiskerCategoryDataset dataset 
-            = new DefaultBoxAndWhiskerCategoryDataset();
+
+        final DefaultBoxAndWhiskerCategoryDataset dataset = new DefaultBoxAndWhiskerCategoryDataset();
         for (int i = 0; i < seriesCount; i++) {
             for (int j = 0; j < categoryCount; j++) {
                 final List list = new ArrayList();
-                // add some values...
+
+                // Here you add the values
                 for (int k = 0; k < entityCount; k++) {
                     final double value1 = 10.0 + Math.random() * 3;
                     list.add(new Double(value1));
-                    final double value2 = 11.25 + Math.random(); // concentrate values in the middle
+                    final double value2 = 11.25 + Math.random(); // concentrate
+                                                                 // values in
+                                                                 // the middle
                     list.add(new Double(value2));
                 }
+
                 dataset.add(list, "Series " + i, " Type " + j);
             }
-            
+
         }
-        
+
         return dataset;
     }
- 
+
     /**
      * Creates a diagram with for each Chunk a BoxPlot.
      * 
@@ -70,42 +84,44 @@ public class BoxPlot {
      *            Table to use
      */
     public static void boxPlot() {
-        //configs
         final String xName = "Type";
         final String yName = "Value";
-        
-        //
-        
-        JFrame frame = new JFrame();
-        frame.setPreferredSize(new java.awt.Dimension(450, 270));
-        
-        //
+        final Dimension frameDimension = new Dimension(500, 500);
+
+        ApplicationFrame frame = new ApplicationFrame("Vidney");
+        frame.setSize(frameDimension);
+
+        // TODO replace sample dataset with real
         final BoxAndWhiskerCategoryDataset dataset = createSampleDataset();
-        //
-        
+
         final CategoryAxis xAxis = new CategoryAxis(xName);
+        xAxis.setLowerMargin(0.20);
+        xAxis.setUpperMargin(0.20);
         final NumberAxis yAxis = new NumberAxis(yName);
         yAxis.setAutoRangeIncludesZero(false);
-        
+
         final BoxAndWhiskerRenderer renderer = new BoxAndWhiskerRenderer();
+        renderer.setMeanVisible(false);
         renderer.setFillBox(true);
         renderer.setBaseToolTipGenerator(new BoxAndWhiskerToolTipGenerator());
-        final CategoryPlot plot = new CategoryPlot(dataset, xAxis, yAxis, renderer);
+        final CategoryPlot plot = new CategoryPlot(dataset, xAxis, yAxis,
+                renderer);
 
-        final JFreeChart chart = new JFreeChart("Box-and-Whisker Demo", new Font("SansSerif", Font.BOLD, 14), plot, true);
+        final JFreeChart chart = new JFreeChart("Boxplot", new Font(
+                "SansSerif", Font.BOLD, 14), plot, true);
         final ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new java.awt.Dimension(450, 270));
         frame.setContentPane(chartPanel);
         frame.setVisible(true);
     }
-    
-    
 
     /**
      * Creates a Map containing frequencies of column values within the Chunk.
      * 
-     * @param chunk the Chunk to use
-     * @param column the column to count
+     * @param chunk
+     *            the Chunk to use
+     * @param column
+     *            the column to count
      * @return a Map containing the frequencies
      */
     private Map<String, Integer> createFrequencyMap(Chunk chunk, String column) {
@@ -128,16 +144,18 @@ public class BoxPlot {
         return freqMap;
     }
 
-//    public static void main(String[] args) {
-//        String filePath = "/home/bjorn/Documents/Context/Health/health/data/data_use/txtData.txt";
-//        String configPath = "/home/bjorn/Documents/Context/Health/health/data/configXmls/admireTxtConfig.xml";
-//
-//        try {
-//            Table table = Input.readTable(filePath, configPath);
-//            // method to test
-//        } catch (IOException | ParserConfigurationException | SAXException
-//                | InputException e) {
-//            System.out.println("Error!");
-//        }
-//    }
+    // public static void main(String[] args) {
+    // String filePath =
+    // "/home/bjorn/Documents/Context/Health/health/data/data_use/txtData.txt";
+    // String configPath =
+    // "/home/bjorn/Documents/Context/Health/health/data/configXmls/admireTxtConfig.xml";
+    //
+    // try {
+    // Table table = Input.readTable(filePath, configPath);
+    // // method to test
+    // } catch (IOException | ParserConfigurationException | SAXException
+    // | InputException e) {
+    // System.out.println("Error!");
+    // }
+    // }
 }
