10dd16df6013718efa6083d88603e96deb327bde
Saffie <bjorn.vdlaan@gmail.com>
2015-06-02 09:51:35 +0200

    Updated FreqBar to work with new Date value of Enum ValueType

--- health/src/main/java/com/health/visuals/FreqBar.java
+++ health/src/main/java/com/health/visuals/FreqBar.java
@@ -1,13 +1,21 @@
 package com.health.visuals;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
 
 import com.health.Chunk;
 import com.health.Column;
 import com.health.Record;
 import com.health.Table;
+import com.health.ValueType;
+import com.health.input.Input;
+import com.health.input.InputException;
 import com.xeiam.xchart.Chart;
 import com.xeiam.xchart.ChartBuilder;
 import com.xeiam.xchart.StyleManager.ChartType;
@@ -21,6 +29,26 @@ import com.xeiam.xchart.SwingWrapper;
  *
  */
 public final class FreqBar {
+
+    /**
+     * Temporary main method used for testing.
+     * @param args
+     */
+    public static void main(String[] args) {
+        String filePath = "/home/bjorn/Documents/Context/Health/health/data/data_use/txtData.txt";
+        String configPath = "/home/bjorn/Documents/Context/Health/health/data/configXmls/admireTxtConfig.xml";
+
+        try {
+            Table table = Input.readTable(filePath, configPath);
+            frequencyBar(table, "date");
+            // method to test
+        } catch (IOException | ParserConfigurationException | SAXException
+                | InputException e) {
+            System.out.println("Error!");
+        }
+    }
+
+
     /**
      * Private constructor to prevent instantiation.
      */
@@ -30,24 +58,30 @@ public final class FreqBar {
     
     /**
      * Generates a Frequency bar diagram.
+     * This variant has no column specified and chooses the last date column in the Table object.
      * @param table Table to use
      */
     public static void frequencyBar(final Table table) {
-        // Check if the Table contains a frequency column
+        // Check if the Table contains a frequency and a date column
         Column freqColumn = null;
         Column dateColumn = null;
         for (Column c : table.getColumns()) {
             if (c.getIsFrequencyColumn()) {
                 freqColumn = c;
             }
-            else if(c.getType() == ValueType.Date) {
-                
+            else if (c.getType() == ValueType.Date) {
+                dateColumn = c;
             }
         }
-        if (freqColumn != null) {
+        //If both exist, format the frequency map based on these columns.
+        if (freqColumn != null && dateColumn != null) {
             Map<String, Integer> freqMap = formatFrequencyMap(table,
-                    freqColumn.getName(), column);
-            makeBarChart(freqMap); 
+                    freqColumn.getName(), dateColumn.getName());
+            makeBarChart(freqMap, dateColumn.getName());
+        }
+        else {
+            //Not good.
+            System.out.println("Table contains either no frequency column or no date column.");
         }
     }
 
@@ -60,10 +94,22 @@ public final class FreqBar {
      *            Column to display frequency of
      */
     public static void frequencyBar(final Table table, final String column) {
-        // Create frequency map based on column
+        // Check if the Table contains a frequency column
+        Column freqColumn = null;
+        for (Column c : table.getColumns()) {
+            if (c.getIsFrequencyColumn()) {
+                freqColumn = c;
+            }
+        }
+        // If the Table contains a frequency column, use it to format the frequency map
+        if (freqColumn != null) {
+            Map<String, Integer> freqMap = formatFrequencyMap(table, freqColumn.getName(), column);
+            makeBarChart(freqMap, column);
+        }
+        // Else if no frequency column exists, count occurrences of values in the specified column
         else {
             Map<String, Integer> freqMap = createFrequencyMap(table, column);
-            makeBarChart(freqMap);
+            makeBarChart(freqMap, column);
         }
     }
 
@@ -132,17 +178,19 @@ public final class FreqBar {
      * @param freqMap
      *            frequency map
      */
-    private static void makeBarChart(final Map<String, Integer> freqMap) {
+    private static void makeBarChart(final Map<String, Integer> freqMap, final String seriesName) {
+        final int frameWidth = 800;
+        final int frameHeight = 600;
         // Convert input data for processing
         ArrayList<String> labels = new ArrayList<String>(freqMap.keySet());
         ArrayList<Integer> frequency = new ArrayList<Integer>(freqMap.values());
 
         // Create Chart
-        Chart chart = new ChartBuilder().chartType(ChartType.Bar).width(800)
-                .height(600).title("Score Histogram").xAxisTitle("Event")
+        Chart chart = new ChartBuilder().chartType(ChartType.Bar).width(frameWidth)
+                .height(frameHeight).title("Score Histogram").xAxisTitle(seriesName)
                 .yAxisTitle("Frequency").build();
 
-        chart.addSeries("Test 1", new ArrayList<String>(labels),
+        chart.addSeries(seriesName, new ArrayList<String>(labels),
                 new ArrayList<Integer>(frequency));
 
         // Customize Chart
