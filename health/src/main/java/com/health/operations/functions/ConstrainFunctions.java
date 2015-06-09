aea89a37825d1dac868de28ba82b126a6b7d7b1c
adamIqbal <hamier08@gmail.com>
2015-06-09 10:44:00 +0200

    checks whether date is equal

--- health/src/main/java/com/health/operations/functions/ConstrainFunctions.java
+++ health/src/main/java/com/health/operations/functions/ConstrainFunctions.java
@@ -1,174 +1,174 @@
 package com.health.operations.functions;
 
+import java.time.LocalDate;
+
+import com.health.script.runtime.DateValue;
 import com.health.script.runtime.NumberValue;
 import com.health.script.runtime.StringValue;
 import com.health.script.runtime.Value;
 
 /**
- * A utility class, which consists of functions which can be used by the script
- * for greater than, greater/eq,smaller than, smaller/eq smaller operations.
+ * A utility class, which consists of functions which can be used by the script for greater than,
+ * greater/eq,smaller than, smaller/eq smaller operations.
  *
  */
 public final class ConstrainFunctions {
-    /**
-     * Function to determine whether the value gained through input is greater
-     * than value in the table.
-     *
-     * @param columns
-     *            original value from the column
-     * @param value
-     *            value which is gained through input
-     * @return boolean if condition is met or exception
-     * @throws IllegalArgumentException
-     *             thrown if there are no valid arguments
-     */
-    public static boolean greater(final Object columns, final Value value) {
-        if (value instanceof NumberValue && columns instanceof Double) {
-            NumberValue d = (NumberValue) value;
+  /**
+   * Function to determine whether the value gained through input is greater than value in the
+   * table.
+   *
+   * @param columns
+   *          original value from the column
+   * @param value
+   *          value which is gained through input
+   * @return boolean if condition is met or exception
+   * @throws IllegalArgumentException
+   *           thrown if there are no valid arguments
+   */
+  public static boolean greater(final Object columns, final Value value) {
+    if (value instanceof NumberValue && columns instanceof Double) {
+      NumberValue d = (NumberValue) value;
 
-            return (double) columns > d.getValue();
-        }
-
-        if (columns instanceof String && value instanceof StringValue) {
-            throw new IllegalArgumentException("Can not invoke greater on String.");
-        }
-
-        // if (columns instanceof Date && value instanceof DateValue) {
-        //
-        // return true;
-        // }
-        // FIXME: add dateValue
-
-        throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+      return (double) columns > d.getValue();
     }
 
-    /**
-     * Function to determine whether the value gained through input is greater
-     * than or equal to value in the table.
-     *
-     * @param columns
-     *            original value from the column
-     * @param value
-     *            value which is gained through input
-     * @return boolean if condition is met or exception
-     * @throws IllegalArgumentException
-     *             thrown if there are no valid arguments
-     */
-    public static boolean greaterEq(final Object columns, final Value value) {
-        if (value instanceof NumberValue && columns instanceof Double) {
-            NumberValue d = (NumberValue) value;
-
-            return (double) columns >= d.getValue();
-        }
-
-        if (columns instanceof String && value instanceof StringValue) {
-            throw new IllegalArgumentException("Can not invoke greatereq on String.");
-        }
-
-        // if (columns instanceof Date && value instanceof DateValue) {
-        //
-        // return true;
-        // }
-        // FIXME: add dateValue
-
-        throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+    if (columns instanceof String && value instanceof StringValue) {
+      throw new IllegalArgumentException("Can not invoke greater on String.");
     }
 
-    /**
-     * Function to determine whether the value gained through input is smaller
-     * than or equal to value in the table.
-     *
-     * @param columns
-     *            original value from the column
-     * @param value
-     *            value which is gained through input
-     * @return boolean if condition is met or exception
-     * @throws IllegalArgumentException
-     *             thrown if there are no valid arguments
-     */
-    public static boolean smallerEq(final Object columns, final Value value) {
-        if (value instanceof NumberValue && columns instanceof Double) {
-            NumberValue d = (NumberValue) value;
-
-            return (double) columns <= d.getValue();
-        }
-
-        if (columns instanceof String && value instanceof StringValue) {
-            throw new IllegalArgumentException("Can not invoke smallereq on String.");
-        }
-
-        // if (columns instanceof Date && value instanceof DateValue) {
-        //
-        // return true;
-        // }
-        // FIXME: add dateValue
-
-        throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+    if (columns instanceof LocalDate && value instanceof DateValue) {
+      LocalDate time = (LocalDate) columns;
+      return time.toString().equals(((DateValue) value).toString());
     }
 
-    /**
-     * Function to determine whether the value gained through input is smaller
-     * than to value in the table.
-     *
-     * @param columns
-     *            original value from the column
-     * @param value
-     *            value which is gained through input
-     * @return boolean if condition is met or exception
-     * @throws IllegalArgumentException
-     *             thrown if there are no valid arguments
-     */
-    public static boolean smaller(final Object columns, final Value value) {
-        if (value instanceof NumberValue && columns instanceof Double) {
-            NumberValue d = (NumberValue) value;
+    throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+  }
 
-            return (double) columns < d.getValue();
-        }
+  /**
+   * Function to determine whether the value gained through input is greater than or equal to value
+   * in the table.
+   *
+   * @param columns
+   *          original value from the column
+   * @param value
+   *          value which is gained through input
+   * @return boolean if condition is met or exception
+   * @throws IllegalArgumentException
+   *           thrown if there are no valid arguments
+   */
+  public static boolean greaterEq(final Object columns, final Value value) {
+    if (value instanceof NumberValue && columns instanceof Double) {
+      NumberValue d = (NumberValue) value;
 
-        if (columns instanceof String && value instanceof StringValue) {
-            throw new IllegalArgumentException("Can not invoke smaller on String.");
-        }
-
-        // if (columns instanceof Date && value instanceof DateValue) {
-        //
-        // return true;
-        // }
-        // FIXME: add dateValue
-
-        throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+      return (double) columns >= d.getValue();
     }
 
-    /**
-     * Function to determine whether the value gained through input is equal to
-     * value in the table.
-     *
-     * @param columns
-     *            original value from the column
-     * @param value
-     *            value which is gained through input
-     * @return boolean if condition is met or exception
-     * @throws IllegalArgumentException
-     *             thrown if there are no valid arguments
-     */
-    public static boolean equal(final Object columns, final Value value) {
-        if (value instanceof NumberValue && columns instanceof Double) {
-            NumberValue d = (NumberValue) value;
-
-            return (double) columns == d.getValue();
-        }
-
-        if (columns instanceof String && value instanceof StringValue) {
-            StringValue str = (StringValue) value;
-
-            return columns.toString().equals(str.getValue());
-        }
-
-        // if (columns instanceof Date && value instanceof DateValue) {
-        //
-        // return true;
-        // }
-        // FIXME: add dateValue
-
-        throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+    if (columns instanceof String && value instanceof StringValue) {
+      throw new IllegalArgumentException("Can not invoke greatereq on String.");
     }
+
+    // if (columns instanceof Date && value instanceof DateValue) {
+    //
+    // return true;
+    // }
+    // FIXME: add dateValue
+
+    throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+  }
+
+  /**
+   * Function to determine whether the value gained through input is smaller than or equal to value
+   * in the table.
+   *
+   * @param columns
+   *          original value from the column
+   * @param value
+   *          value which is gained through input
+   * @return boolean if condition is met or exception
+   * @throws IllegalArgumentException
+   *           thrown if there are no valid arguments
+   */
+  public static boolean smallerEq(final Object columns, final Value value) {
+    if (value instanceof NumberValue && columns instanceof Double) {
+      NumberValue d = (NumberValue) value;
+
+      return (double) columns <= d.getValue();
+    }
+
+    if (columns instanceof String && value instanceof StringValue) {
+      throw new IllegalArgumentException("Can not invoke smallereq on String.");
+    }
+
+    // if (columns instanceof Date && value instanceof DateValue) {
+    //
+    // return true;
+    // }
+    // FIXME: add dateValue
+
+    throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+  }
+
+  /**
+   * Function to determine whether the value gained through input is smaller than to value in the
+   * table.
+   *
+   * @param columns
+   *          original value from the column
+   * @param value
+   *          value which is gained through input
+   * @return boolean if condition is met or exception
+   * @throws IllegalArgumentException
+   *           thrown if there are no valid arguments
+   */
+  public static boolean smaller(final Object columns, final Value value) {
+    if (value instanceof NumberValue && columns instanceof Double) {
+      NumberValue d = (NumberValue) value;
+
+      return (double) columns < d.getValue();
+    }
+
+    if (columns instanceof String && value instanceof StringValue) {
+      throw new IllegalArgumentException("Can not invoke smaller on String.");
+    }
+
+    // if (columns instanceof Date && value instanceof DateValue) {
+    //
+    // return true;
+    // }
+    // FIXME: add dateValue
+
+    throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+  }
+
+  /**
+   * Function to determine whether the value gained through input is equal to value in the table.
+   *
+   * @param columns
+   *          original value from the column
+   * @param value
+   *          value which is gained through input
+   * @return boolean if condition is met or exception
+   * @throws IllegalArgumentException
+   *           thrown if there are no valid arguments
+   */
+  public static boolean equal(final Object columns, final Value value) {
+    if (value instanceof NumberValue && columns instanceof Double) {
+      NumberValue d = (NumberValue) value;
+
+      return (double) columns == d.getValue();
+    }
+
+    if (columns instanceof String && value instanceof StringValue) {
+      StringValue str = (StringValue) value;
+
+      return columns.toString().equals(str.getValue());
+    }
+
+    if (columns instanceof LocalDate && value instanceof DateValue) {
+      LocalDate time = (LocalDate) columns;
+      return time.toString().equals(((DateValue) value).toString());
+    }
+
+    throw new IllegalArgumentException("Expected valuetype && constraintype to be valid.");
+  }
 }
