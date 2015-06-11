78333327323151d62bfc06f0a161a2dde3fa2185
LizzyScholten <lizzyxmissxsportyx@hotmail.com>
2015-06-11 10:21:53 +0200

    State Transition Matrix can now be created with dummy input. Next step is the visualisation of the matrix

--- health/src/main/java/com/health/visuals/StateTransitionMatrix.java
+++ health/src/main/java/com/health/visuals/StateTransitionMatrix.java
@@ -25,116 +25,154 @@ public final class StateTransitionMatrix {
 	}
 	
 	public static void main(String[] args) {
-		/*
-		
-		Input:
-		List EventSequences, pattern, subset to check within entire event sequence set
-		
-		*/
 		
 		//Create dummy input values
-		Table table;
-    
-        List<Column> columns = new ArrayList<Column>();
-        columns.add(new Column("date", 0, ValueType.Date));
-        columns.add(new Column("waarde", 1, ValueType.Number));
-        columns.add(new Column("name", 2, ValueType.String));
+			Table table;
+	    
+	        List<Column> columns = new ArrayList<Column>();
+	        columns.add(new Column("date", 0, ValueType.Date));
+	        columns.add(new Column("waarde", 1, ValueType.Number));
+	        columns.add(new Column("name", 2, ValueType.String));
 
-        table = new Table(columns);
+	        table = new Table(columns);
 
-        Record tmp = new Record(table);
-        tmp.setValue(0, LocalDate.of(1, 1, 1));
-        tmp.setValue(1, 1.0);
-        tmp.setValue(2, "piet");
+	        Record tmp = new Record(table);
+	        tmp.setValue(0, LocalDate.of(1, 1, 1));
+	        tmp.setValue(1, 1.0);
+	        tmp.setValue(2, "piet");
 
-        tmp = new Record(table);
-        tmp.setValue(0, LocalDate.of(2, 2, 2));
-        tmp.setValue(1, 2.0);
-        tmp.setValue(2, "Jan");
+	        tmp = new Record(table);
+	        tmp.setValue(0, LocalDate.of(2, 2, 2));
+	        tmp.setValue(1, 2.0);
+	        tmp.setValue(2, "Jan");
 
-        tmp = new Record(table);
-        tmp.setValue(0, LocalDate.of(3, 2, 3));
-        tmp.setValue(1, 3.0);
-        tmp.setValue(2, "piet");
+	        tmp = new Record(table);
+	        tmp.setValue(0, LocalDate.of(3, 2, 3));
+	        tmp.setValue(1, 3.0);
+	        tmp.setValue(2, "piet");
 
-        tmp = new Record(table);
-        tmp.setValue(0, LocalDate.of(4, 2, 2));
-        tmp.setValue(1, 4.0);
-        tmp.setValue(2, "piet");
-        
-        tmp = new Record(table);
-        tmp.setValue(0, LocalDate.of(4, 2, 3));
-        tmp.setValue(1, 2.0);
-        tmp.setValue(2, "Jan");
-    
-        EventList eList = new EventList();
+	        tmp = new Record(table);
+	        tmp.setValue(0, LocalDate.of(4, 2, 2));
+	        tmp.setValue(1, 4.0);
+	        tmp.setValue(2, "piet");
+	        
+	        tmp = new Record(table);
+	        tmp.setValue(0, LocalDate.of(4, 2, 3));
+	        tmp.setValue(1, 2.0);
+	        tmp.setValue(2, "Jan");
+	    
+	        EventList eList = new EventList();
 
-        Event e1 = new Event("A", table.getRecords().get(0));
-        Event e2 = new Event("B", table.getRecords().get(1));
-        Event e3 = new Event("A", table.getRecords().get(2));
-        Event e4 = new Event("B", table.getRecords().get(3));
-        Event e5 = new Event("A", table.getRecords().get(4));
+	        Event e1 = new Event("A", table.getRecords().get(0));
+	        Event e2 = new Event("B", table.getRecords().get(1));
+	        Event e3 = new Event("A", table.getRecords().get(2));
+	        Event e4 = new Event("B", table.getRecords().get(3));
+	        Event e5 = new Event("A", table.getRecords().get(4));
+	        Event e6 = new Event("C", table.getRecords().get(4));
 
-        eList.addEvent(e1);
-        eList.addEvent(e2);
-        eList.addEvent(e3);
-        eList.addEvent(e4);
-        eList.addEvent(e5);
-        
-        String[] codePat1 = {"B", "A", "A"};
-        String[] codePat2 = {"A", "B",};
-        String[] codePat3 = {"A", "B", "A"};
-        
-        EventSequence eSeq1 = new EventSequence(codePat1);
-        EventSequence eSeq2 = new EventSequence(codePat2);
-        EventSequence eSeq3 = new EventSequence(codePat3);
-        
-        List<EventSequence> listOfSeq = new ArrayList<EventSequence>();
-        
-        listOfSeq.add(eSeq1);
-        listOfSeq.add(eSeq2);
-        listOfSeq.add(eSeq3);
-        
-		createStateTrans(eList, listOfSeq);
+	        eList.addEvent(e1);
+	        eList.addEvent(e2);
+	        eList.addEvent(e3);
+	        eList.addEvent(e4);
+	        eList.addEvent(e5);
+	        eList.addEvent(e6);
+	        
+	        String[] codePat1 = {"B", "A", "A", "B"};
+	        String[] codePat2 = {"A", "A", "B"};
+	        String[] codePat3 = {"A", "B", "A", "C"};
+	        String[] codePat4 = {"C", "B", "C"};
+	        
+	        EventSequence eSeq1 = new EventSequence(codePat1);
+	        EventSequence eSeq2 = new EventSequence(codePat2);
+	        EventSequence eSeq3 = new EventSequence(codePat3);
+	        EventSequence eSeq4 = new EventSequence(codePat4);
+	        
+	        List<EventSequence> listOfSeq = new ArrayList<EventSequence>();
+	        
+	        listOfSeq.add(eSeq1);
+	        listOfSeq.add(eSeq2);
+	        listOfSeq.add(eSeq3);
+	        listOfSeq.add(eSeq4);
+	        
+			StateTransitionMatrix.createStateTrans(eList, listOfSeq);
+	
 	}
 	
 	
 	public static void createStateTrans(EventList eList, List<EventSequence> seqList){
 		
-		String[] events = new String[eList.getList().size()];
-		int count = 0;
-		boolean found = false;
+		ArrayList<String> eArr = new ArrayList<String>();
 		
 		for(Event e: eList.getList()){
-			for(int i = 0; i < events.length; i++){
-				if(e.getCode().equals(events[i])){
-					found = true;
-				}
+			if(eArr.contains(e.getCode())){
+			
 			}
-			if(!found){
-				events[count] = e.getCode();
-				count = count + 1;
+			else {
+				eArr.add(e.getCode());
 			}
-			found = false;
 		}
 		
-		for(int t = 0; t < events.length; t++){
-			System.out.println(events[t]);
-		}
+		for(String s: eArr){
+			System.out.println(s);
+		}	
 		
+		String[][] matrix = new String[eArr.size()+1][eArr.size()+1];
 		
+		matrix[0][0] = "Event types";
 		
-		String[][] matrix = new String[events.length+1][events.length+1];
-		int count2 = 0;
-		
-		for(int k = 1; k < events.length+1; k++){
-			matrix[0][k] = events[count2];
-			matrix[k][0] = events[count2];
-			count2 = count2 + 1;
+		for(int k = 1; k < eArr.size()+1; k++){
+			matrix[0][k] = eArr.get(k-1);
+			matrix[k][0] = eArr.get(k-1);
 		}
 		
 		System.out.println(Arrays.deepToString(matrix));
+			
 		
-	}
+		//B A A
+		//A B
+		//A B A
+		for(EventSequence eSeq: seqList){
+			String[] codePat = eSeq.getCodePattern();
+			
+			for(int c = 1; c < codePat.length; c++){
+				String from = codePat[c-1];
+				String to = codePat[c];
+				
+				for(int a = 1; a < matrix[0].length; a++){
+					boolean found1 = false;
+					
+					System.out.println("matrix a0 : "+ matrix[a][0]);
+					System.out.println("from : "+ from);
+					
+					if(matrix[a][0].equals(from)){
+						for(int b = 1; b < matrix[1].length; b++){
+							System.out.println("matrix 0b : "+ matrix[0][b]);
+							System.out.println("to : "+ to);
+							if(matrix[0][b].equals(to)){
+								if (matrix[a][b] == null) {
+									matrix[a][b] = "1";
+								}
+								else {
+									int val = Integer.parseInt(matrix[a][b]);
+									val = val + 1;
+									matrix[a][b] = Integer.toString(val);
+								}
+								found1 = true;
+								break;
+							}		
+						}
+					}
+					
+					if(found1){
+						break;
+					}
+				}
+			}
+			
+			
+		}	
+		
+		System.out.println(Arrays.deepToString(matrix));
 	
+	}	
 }
