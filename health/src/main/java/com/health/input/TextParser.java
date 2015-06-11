77c7568435498395a761fd2e7735f4bcbfd8b6ff
adamIqbal <hamier08@gmail.com>
2015-06-11 12:19:50 +0200

    made a special class for input functions as ignore last

--- health/src/main/java/com/health/input/TextParser.java
+++ health/src/main/java/com/health/input/TextParser.java
@@ -90,28 +90,6 @@ public final class TextParser implements Parser {
     return table;
   }
 
-  /**
-   * Deletes the last x lines which are not needed as specified by the user.
-   * 
-   * @param table
-   *          Gets the table with the redundant lines.
-   * @param config
-   *          Gets the InputDescriptor which is used to create the original file.
-   * @return the new table with deleted lines.
-   */
-  private Table deleteLastLines(final Table table, final InputDescriptor config) {
-
-    int deletions = config.getIgnoreLast();
-    int size = table.size();
-    List<Record> tab = table.getRecords();
-    while (deletions > 0) {
-      table.removeRecord(tab.get(size));
-      size--;
-      deletions--;
-    }
-    return table;
-  }
-
   private static Double stringToNumber(final String value, final int lineNumber)
       throws InputException {
     try {
