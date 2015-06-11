e1f2f00d376b8d8e1bee0e824e7986a67370e222
adamIqbal <adamIqbal@users.noreply.github.com>
2015-06-11 21:01:46 +0200

    Revert "xls Parser fix for ignore last lines"

--- health/src/main/java/com/health/input/TextParser.java
+++ health/src/main/java/com/health/input/TextParser.java
@@ -85,8 +85,30 @@ public final class TextParser implements Parser {
       }
     }
 
-    table = InputFunctions.deleteLastLines(table, config);
+    table = deleteLastLines(table, config);
 
+    return table;
+  }
+
+  /**
+   * Deletes the last x lines which are not needed as specified by the user.
+   * 
+   * @param table
+   *          Gets the table with the redundant lines.
+   * @param config
+   *          Gets the InputDescriptor which is used to create the original file.
+   * @return the new table with deleted lines.
+   */
+  private Table deleteLastLines(final Table table, final InputDescriptor config) {
+
+    int deletions = config.getIgnoreLast();
+    int size = table.size();
+    List<Record> tab = table.getRecords();
+    while (deletions > 0) {
+      table.removeRecord(tab.get(size));
+      size--;
+      deletions--;
+    }
     return table;
   }
 
