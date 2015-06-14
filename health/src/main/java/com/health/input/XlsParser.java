030659afef9e87519eea371f16f96b9876973b2a
daanvermunt <daanvermunt@gmail.com>
2015-06-14 20:33:33 +0200

    fixed for xls parser and added testcase

--- health/src/main/java/com/health/input/XlsParser.java
+++ health/src/main/java/com/health/input/XlsParser.java
@@ -2,6 +2,7 @@ package com.health.input;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -10,6 +11,8 @@ import java.util.Objects;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.health.Record;
 import com.health.Table;
@@ -20,87 +23,119 @@ import com.health.Table;
  */
 public final class XlsParser implements Parser {
 
-	/**
-	 * Given a path to a xls file and an input descriptor, parses the input file
-	 * into a {@link Table}.
-	 *
-	 * @param path
-	 *            the path of the input file.
-	 * @param config
-	 *            the input descriptor.
-	 * @return a table representing the parsed input file.
-	 * @throws IOException
-	 *             if any IO errors occur.
-	 * @throws InputException
-	 *             if any input errors occur.
-	 */
+    /**
+     * Given a path to a xls file and an input descriptor, parses the input file
+     * into a {@link Table}.
+     *
+     * @param path
+     *            the path of the input file.
+     * @param config
+     *            the input descriptor.
+     * @return a table representing the parsed input file.
+     * @throws IOException
+     *             if any IO errors occur.
+     * @throws InputException
+     *             if any input errors occur.
+     */
 
-	@Override
-	public Table parse(final String path, final InputDescriptor config)
-			throws InputException, IOException {
-		Objects.requireNonNull(path);
-		Objects.requireNonNull(config);
+    @Override
+    public Table parse(final String path, final InputDescriptor config)
+            throws InputException, IOException {
+        Objects.requireNonNull(path);
+        Objects.requireNonNull(config);
 
-		Table table = config.buildTable();
+        Table table = config.buildTable();
 
-		FileInputStream io = new FileInputStream(path);
-		HSSFWorkbook wb = new HSSFWorkbook(io);
+        FileInputStream io = new FileInputStream(path);
 
-		StartCell startCell = config.getStartCell();
-		int rowCount = 0;
-		int columnsCount = config.getColumns().size();
+        String ext = getFileExtension(path);
+        Workbook wb;
+        System.out.println(ext);
+        if (ext.equals(".xls")) {
+            wb = new HSSFWorkbook(io);
+        } else if (ext.equals(".xlsx")) {
+            wb = new XSSFWorkbook(io);
+        } else {
+            io.close();
+            throw new InputException(
+                    "Not a xls or xlsx file, so cannot parse with XLSParser");
+        }
 
-		Sheet sheet = wb.getSheetAt(0);
-		for (Row row : sheet) {
-			// if at start row or beyond
-			if (rowCount >= startCell.getStartRow()) {
-				Record tableRow = new Record(table);
+        StartCell startCell = config.getStartCell();
+        int rowCount = 0;
+        int columnsCount = config.getColumns().size();
 
-				int columnCountTableRow = 0;
-				for (int i = startCell.getStartColumn() - 1; i < columnsCount
-						+ startCell.getStartColumn() - 1; i++) {
-					switch (table.getColumn(columnCountTableRow).getType()) {
-					case String:
-						tableRow.setValue(columnCountTableRow, row.getCell(i)
-								.toString());
-						break;
-					case Number:
-						tableRow.setValue(columnCountTableRow,
-								Double.parseDouble(row.getCell(i).toString()));
-						break;
-					case Date:
-						if (config.getDateFormat() != null) {
-							try {
-								DateTimeFormatter formatter = DateTimeFormatter
+        Sheet sheet = wb.getSheetAt(0);
+        for (Row row : sheet) {
+            // if at start row or beyond
+            if (rowCount >= startCell.getStartRow()) {
+                Record tableRow = new Record(table);
 
-								.ofPattern(config.getDateFormat());
-								LocalDate dateValue = LocalDate.parse(row
-										.getCell(i).toString(), formatter);
-								tableRow.setValue(columnCountTableRow,
-										dateValue);
-							} catch (DateTimeParseException e) {
-								break;
-							}
-						}
-						break;
-					default:
-						// The type was null, this should never happen
-						assert false;
-						throw new InputException(
-								"Internal error.",
-								new Exception("Column.getType() returned null."));
-					}
+                int columnCountTableRow = 0;
+                for (int i = startCell.getStartColumn() - 1; i < columnsCount
+                        + startCell.getStartColumn() - 1; i++) {
+                    String value;
+                    try {
+                        value = row.getCell(i).toString();
+                    } catch (NullPointerException e) {
+                        value = "NULL";
+                    }
 
-					columnCountTableRow++;
-				}
+                    switch (table.getColumn(columnCountTableRow).getType()) {
+                    case String:
+                        if (!value.equals("NULL")) {
+                            tableRow.setValue(columnCountTableRow, value);
+                        }
+                        break;
+                    case Number:
+                        if (!value.equals("NULL")) {
+                            tableRow.setValue(columnCountTableRow,
+                                    Double.parseDouble(value));
+                        }
+                        break;
+                    case Date:
+                        if (config.getDateFormat() != null) {
+                            try {
+                                DateTimeFormatter formatter = DateTimeFormatter
 
-			}
+                                .ofPattern(config.getDateFormat());
+                                LocalDate dateValue = LocalDate.parse(row
+                                        .getCell(i).toString(), formatter);
+                                tableRow.setValue(columnCountTableRow,
+                                        dateValue);
+                            } catch (DateTimeParseException e) {
+                                break;
+                            }
+                        }
+                        break;
+                    default:
+                        // The type was null, this should never happen
+                        assert false;
+                        throw new InputException(
+                                "Internal error.",
+                                new Exception("Column.getType() returned null."));
+                    }
 
-			rowCount++;
-		}
+                    columnCountTableRow++;
+                }
 
-		wb.close();
-		return table;
+            }
 
-	}
+            rowCount++;
+        }
+
+        wb.close();
+        return table;
+
+    }
+
+    private String getFileExtension(String path) {
+        try {
+            return path.substring(path.lastIndexOf("."));
+
+        } catch (Exception e) {
+            return "";
+        }
+
+    }
 }
