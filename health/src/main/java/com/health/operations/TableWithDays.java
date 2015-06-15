1be7af05776f25fdeb4c27d346f9e8254be2bb15
adamIqbal <hamier08@gmail.com>
2015-06-15 11:31:16 +0200

    adds records to new table

--- health/src/main/java/com/health/operations/TableWithDays.java
+++ health/src/main/java/com/health/operations/TableWithDays.java
@@ -1,11 +1,33 @@
 package com.health.operations;
 
+import java.util.List;
+
+import com.health.Column;
+import com.health.Record;
 import com.health.Table;
 
 public class TableWithDays {
 
   public static Table TableDays(final Table table) {
-    
+    Table origTable = table;
+    List<Record> recList = origTable.getRecords();
+    List<Column> colList = origTable.getColumns();
+
+    Table result = null;
+
+    for (int i = 0; i < recList.size(); i++) {
+
+      Record record = new Record(result);
+
+      colList.add(new Column("day_of_week", colList.size(), null));
+
+      for (Column column : colList) {
+        String name = column.getName();
+        String newName = name;
+
+        record.setValue(name, recList.get(i).getValue(newName));
+      }
+    }
 
     return null;
 
