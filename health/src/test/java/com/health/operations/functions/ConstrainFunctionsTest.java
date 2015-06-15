509de5e2ac18a9831ce34eee9d7df399ee70a2f6
adamIqbal <hamier08@gmail.com>
2015-06-15 10:15:27 +0200

    tested number inputs for constrainfunctions

--- health/src/test/java/com/health/operations/functions/ConstrainFunctionsTest.java
+++ health/src/test/java/com/health/operations/functions/ConstrainFunctionsTest.java
@@ -3,18 +3,26 @@ package com.health.operations.functions;
 import static org.junit.Assert.*;
 import static com.health.operations.functions.ConstrainFunctions.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.health.script.runtime.NumberValue;
 import com.health.script.runtime.Value;
 
 public class ConstrainFunctionsTest {
+  double columns;
+  NumberValue value;
+
+  @Before
+  public void setUp() {
+
+    columns = 3;
+    value = new NumberValue();
+    value.setValue(2);
+  }
 
   @Test
   public void testGreaterNumber() {
-    double columns = 3;
-    NumberValue value = new NumberValue();
-    value.setValue(2);
 
     assertTrue(greater((Object) columns, (Value) value));
 
@@ -31,74 +39,145 @@ public class ConstrainFunctionsTest {
     assertTrue(greater((Object) columns, (Value) value));
   }
 
-//  @Test
-//  public void testGreaterEqNumber() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testSmallerEqNumber() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testSmallerNumber() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testEqualNumber() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testGreaterDate() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testGreaterEqDate() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testSmallerEqDate() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testSmallerDate() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testEqualDate() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testGreaterString() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testGreaterEqString() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testSmallerEqString() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testSmallerString() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  public void testEqualString() {
-//    fail("Not yet implemented");
-//  }
+  @Test
+  public void testGreaterEqNumber() {
+    assertTrue(greaterEq((Object) columns, (Value) value));
+
+    value.setValue(3);
+
+    assertTrue(greaterEq((Object) columns, (Value) value));
+    assertFalse(greaterEq((Object) columns, null));
+    assertFalse(greaterEq(null, (Value) value));
+
+    columns = 4;
+
+    assertTrue(greaterEq((Object) columns, (Value) value));
+    value.setValue(4);
+    assertTrue(greaterEq((Object) columns, (Value) value));
+
+    columns = 1;
+
+    assertFalse(greaterEq((Object) columns, (Value) value));
+
+  }
+
+  @Test
+  public void testSmallerEqNumber() {
+    assertTrue(greater((Object) columns, (Value) value));
+
+    value.setValue(3);
+
+    assertTrue(smallerEq((Object) columns, (Value) value));
+
+    assertTrue(smallerEq((Object) columns, (Value) value));
+    assertFalse(smallerEq((Object) columns, null));
+    assertFalse(smallerEq(null, (Value) value));
+
+    columns = 4;
+
+    assertFalse(smallerEq((Object) columns, (Value) value));
+    value.setValue(4);
+    assertTrue(smallerEq((Object) columns, (Value) value));
+
+    columns = 1;
+
+    assertTrue(smallerEq((Object) columns, (Value) value));
+  }
+
+  @Test
+  public void testSmallerNumber() {
+    assertFalse(smaller((Object) columns, (Value) value));
+
+    value.setValue(3);
+
+    assertFalse(smaller((Object) columns, (Value) value));
+
+    assertFalse(smaller((Object) columns, (Value) value));
+    assertFalse(smaller((Object) columns, null));
+    assertFalse(smaller(null, (Value) value));
+
+    columns = 400;
+
+    assertFalse(smaller((Object) columns, (Value) value));
+    value.setValue(400);
+    assertFalse(smaller((Object) columns, (Value) value));
+
+    columns = 100;
+
+    assertTrue(smaller((Object) columns, (Value) value));
+  }
+
+  @Test
+  public void testEqualNumber() {
+    assertFalse(equal((Object) columns, (Value) value));
+
+    value.setValue(3);
+
+    assertTrue(equal((Object) columns, (Value) value));
+
+    assertTrue(equal((Object) columns, (Value) value));
+    assertFalse(equal((Object) columns, null));
+    assertFalse(equal(null, (Value) value));
+
+    columns = 10;
+
+    assertFalse(equal((Object) columns, (Value) value));
+    value.setValue(10);
+    assertTrue(equal((Object) columns, (Value) value));
+
+    columns = 1;
+
+    assertFalse(equal((Object) columns, (Value) value));
+  }
+
+  // @Test
+  // public void testGreaterDate() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // @Test
+  // public void testGreaterEqDate() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // @Test
+  // public void testSmallerEqDate() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // @Test
+  // public void testSmallerDate() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // @Test
+  // public void testEqualDate() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // @Test
+  // public void testGreaterString() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // @Test
+  // public void testGreaterEqString() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // @Test
+  // public void testSmallerEqString() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // @Test
+  // public void testSmallerString() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // @Test
+  // public void testEqualString() {
+  // fail("Not yet implemented");
+  // }
 
 }
