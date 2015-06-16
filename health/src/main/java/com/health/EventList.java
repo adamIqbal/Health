b21023a575214bf73cb48b1ea3f1fb597477c365
Martijn van Dorp <martijnvandorp@casema.nl>
2015-06-16 10:43:53 +0200

    Fixed typo in sorting EventList by date.

--- health/src/main/java/com/health/EventList.java
+++ health/src/main/java/com/health/EventList.java
@@ -56,53 +56,54 @@ public class EventList {
             } else if (dateA != null && dateB == null) {
                 return 1;
             } else {
-                return dateA.compareTo(dateA);
+                return dateA.compareTo(dateB);
             }
         });
 
         eventList = sortedEvents;
     }
-    
+
     /**
      * Turns this eventList into a table.
+     * 
      * @return a Table format with all element in the eventSequence
      */
     public Table toTable() {
-        if(this.eventList.size() <= 0){
+        if (this.eventList.size() <= 0) {
             return null;
         }
         List<Column> cols = makeTableCols();
         Table res = new Table(cols);
         addAllRecords(res, cols);
-                
+
         return res;
     }
-    
-    private List<Column> makeTableCols(){
+
+    private List<Column> makeTableCols() {
         List<Column> cols = new ArrayList<Column>();
-        
-        for(Event e : this.eventList){
+
+        for (Event e : this.eventList) {
             List<Column> colsE = e.getRecord().getTable().getColumns();
-            for(Column c : colsE){
-                if(!cols.contains(c)){
+            for (Column c : colsE) {
+                if (!cols.contains(c)) {
                     cols.add(c);
                 }
             }
         }
-        
+
         return cols;
     }
-    
-    private void addAllRecords(Table table, List<Column> cols){        
-        for(Event e : this.eventList){
+
+    private void addAllRecords(Table table, List<Column> cols) {
+        for (Event e : this.eventList) {
             Record rec = e.getRecord();
-            
+
             Record newRec = new Record(table);
-            for(Column c : cols){
+            for (Column c : cols) {
                 Object val = rec.getValue(c.getName());
-                if(val != null){
+                if (val != null) {
                     newRec.setValue(c.getName(), val);
-                }                
+                }
             }
         }
     }
