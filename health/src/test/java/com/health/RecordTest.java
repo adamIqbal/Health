e9036e6a90d188567224ae2d35b1c8f98330bb57
Martijn van Dorp <martijnvandorp@casema.nl>
2015-06-17 11:53:02 +0200

    Updated unit tests for Record.

--- health/src/test/java/com/health/RecordTest.java
+++ health/src/test/java/com/health/RecordTest.java
@@ -10,11 +10,13 @@ import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -26,8 +28,10 @@ import org.powermock.modules.junit4.PowerMockRunner;
 public class RecordTest {
     private static final String value1 = "abc";
     private static final Double value2 = 1.5;
+    private static final LocalDate value3 = LocalDate.of(1970, 1, 1);
     private Column column1;
     private Column column2;
+    private Column column3;
     private Record record;
     private Table table;
 
@@ -47,21 +51,31 @@ public class RecordTest {
         when(column2.getIndex()).thenReturn(1);
         when(column2.getName()).thenReturn("column2");
         when(column2.getType()).thenReturn(ValueType.Number);
-        when(column2.isFrequencyColumn()).thenReturn(true);
+        when(column2.isFrequencyColumn()).thenReturn(false);
+
+        column3 = mock(Column.class);
+        when(column3.getIndex()).thenReturn(2);
+        when(column3.getName()).thenReturn("column3");
+        when(column3.getType()).thenReturn(ValueType.Date);
+        when(column3.isFrequencyColumn()).thenReturn(false);
 
         // Create mock table
         table = mock(Table.class);
         when(table.getColumn(anyString())).thenReturn(null);
-        when(table.getColumn(column1.getName())).thenReturn(
-                column1);
-        when(table.getColumn(column2.getName())).thenReturn(
-                column2);
-        when(table.getColumns()).thenReturn(Arrays.asList(column1, column2));
+        when(table.getColumn(column1.getIndex())).thenReturn(column1);
+        when(table.getColumn(column2.getIndex())).thenReturn(column2);
+        when(table.getColumn(column3.getIndex())).thenReturn(column3);
+        when(table.getColumn(column1.getName())).thenReturn(column1);
+        when(table.getColumn(column2.getName())).thenReturn(column2);
+        when(table.getColumn(column3.getName())).thenReturn(column3);
+        when(table.getColumns()).thenReturn(Arrays.asList(column1, column2, column3));
+        when(table.getDateColumn()).thenReturn(column3);
 
         // Create mock record
         record = new Record(table);
         record.setValue(column1.getName(), value1);
         record.setValue(column2.getName(), value2);
+        record.setValue(column3.getName(), value3);
     }
 
     /**
@@ -95,7 +109,7 @@ public class RecordTest {
     public void constructor_givenTable_setsValues() {
         Record record = new Record(table);
 
-        assertThat(record.getValues(), iterableWithSize(2));
+        assertThat(record.getValues(), iterableWithSize(3));
     }
 
     /**
@@ -139,6 +153,16 @@ public class RecordTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void getNumberValue_givenNameOfColumnWithNumberNull_returnsZero() {
+        record.setValue(column2.getName(), (Double) null);
+
+        Double expected = (Double) 0.0;
+        Double actual = record.getNumberValue(column2.getName());
+
+        assertEquals(expected, actual);
+    }
+
     /**
      * Tests whether {@link Record#getNumberValue(String)} returns the correct
      * {@link Double} when given the name of a column that contains Doubles.
@@ -159,6 +183,16 @@ public class RecordTest {
     @Test(expected = IllegalStateException.class)
     public void getNumberValue_givenNameOfColumnWithString_throwsIllegalStateException() {
         record.getNumberValue(column1.getName());
+    }
+
+    /**
+     * Tests whether {@link Record#getNumberValue(String)} throws an
+     * {@link IllegalStateException} when given the name of a column that
+     * contains {@link LocalDate}s.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void getNumberValue_givenNameOfColumnWithDate_throwsIllegalStateException() {
+        record.getNumberValue(column3.getName());
     }
 
     /**
@@ -184,28 +218,107 @@ public class RecordTest {
     }
 
     /**
-     * Tests whether {@link Record#setValue(String, Double)} throws a
+     * Tests whether {@link Record#getStringValue(String)} throws an
+     * {@link IllegalStateException} when given the name of a column that
+     * contains {@link LocalDate}s.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void getStringValue_givenNameOfColumnWithDate_throwsIllegalStateException() {
+        record.getStringValue(column3.getName());
+    }
+
+    /**
+     * Tests whether {@link Record#getDateValue(String)} returns the correct
+     * {@link LocalDate} when given the name of a column that contains Strings.
+     */
+    @Test
+    public void getDateValue_givenNameOfColumnWithDate_returnsDate() {
+        LocalDate expected = (LocalDate) value3;
+        LocalDate actual = record.getDateValue(column3.getName());
+
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Tests whether {@link Record#getDateValue(String)} throws an
+     * {@link IllegalStateException} when given the name of a column that
+     * contains {@link Double}s.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void getDateValue_givenNameOfColumnWithNumber_throwsIllegalStateException() {
+        record.getDateValue(column2.getName());
+    }
+
+    /**
+     * Tests whether {@link Record#getDateValue(String)} throws an
+     * {@link IllegalStateException} when given the name of a column that
+     * contains {@link String}s.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void getDateValue_givenNameOfColumnWithString_throwsIllegalStateException() {
+        record.getDateValue(column1.getName());
+    }
+
+    /**
+     * Tests whether {@link Record#setValue(int, Object)} throws a
+     * {@link IllegalArgumentException} when given an index of a column that
+     * does not exist.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void setValue_givenIndexOfNonExistingColumn_throwsIllegalArgumentException() {
+        record.setValue(-1, null);
+    }
+
+    /**
+     * Tests whether {@link Record#setValue(int, Double)} updates the value of
+     * the correct column when given a {@link Double} value and the name of a
+     * column that contains {@link Double}s.
+     */
+    @Test
+    public void setValueDouble_givenIndexOfColumnWithNumber_updatesValue() {
+        Double value = -1.0;
+
+        record.setValue(column2.getIndex(), value);
+
+        Double expected = value;
+        Double actual = record.getNumberValue(column2.getName());
+
+        assertSame(expected, actual);
+    }
+
+    /**
+     * Tests whether {@link Record#setValue(String, Object)} throws a
      * {@link IllegalArgumentException} when given a null reference for name.
      */
     @Test(expected = IllegalArgumentException.class)
-    public void setValueDouble_givenNameNull_throwsIllegalArgumentException() {
+    public void setValue_givenNameNull_throwsIllegalArgumentException() {
         record.setValue((String) null, value2);
     }
 
     /**
-     * Tests whether {@link Record#setValue(String, Double)} throws an
+     * Tests whether {@link Record#setValue(String, Object)} throws an
      * {@link IllegalArgumentException} when given a name of an nonexistent
      * column.
      */
     @Test(expected = IllegalArgumentException.class)
-    public void setValueDouble_givenNameOfNonexistentColumn_throwsIllegalArgumentException() {
+    public void setValue_givenNameOfNonexistentColumn_throwsIllegalArgumentException() {
         record.setValue("null", value2);
     }
 
     /**
-     * Tests whether {@link Record#setValue(String, Double)} throws an
-     * {@link IllegalStateException} when given a name of a column that contains
-     * {@link String}s.
+     * Tests whether {@link Record#setValue(String, Object)} throws an
+     * {@link IllegalArgumentException} when given a value that is not one of
+     * {@link Double}, {@link String} or {@link LocalDate}.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void setValue_givenValueOfInvalidType_throwsIllegalArgumentException() {
+        record.setValue(column1.getName(), new Object());
+    }
+
+    /**
+     * Tests whether {@link Record#setValue(String, Object)} throws an
+     * {@link IllegalStateException} when given a {@link Double} value and the
+     * name of a column that contains {@link String}s.
      */
     @Test(expected = IllegalStateException.class)
     public void setValueDouble_givenNameOfColumnWithString_throwsIllegalStateException() {
@@ -213,9 +326,19 @@ public class RecordTest {
     }
 
     /**
+     * Tests whether {@link Record#setValue(String, Object)} throws an
+     * {@link IllegalStateException} when given a {@link Double} value and the
+     * name of a column that contains {@link LocalDate}s.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void setValueDouble_givenNameOfColumnWithDate_throwsIllegalStateException() {
+        record.setValue(column3.getName(), value2);
+    }
+
+    /**
      * Tests whether {@link Record#setValue(String, Double)} updates the value
-     * of the correct column when given the name of a column that contains
-     * {@link Double}s.
+     * of the correct column when given a {@link Double} value and the name of a
+     * column that contains {@link Double}s.
      */
     @Test
     public void setValueDouble_givenNameOfColumn_updatesValue() {
@@ -230,28 +353,9 @@ public class RecordTest {
     }
 
     /**
-     * Tests whether {@link Record#setValue(String, String)} throws a
-     * {@link IllegalArgumentException} when given a null reference for name.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void setValueString_givenNameNull_throwsIllegalArgumentException() {
-        record.setValue((String) null, value1);
-    }
-
-    /**
-     * Tests whether {@link Record#setValue(String, String)} throws an
-     * {@link IllegalArgumentException} when given a name of an nonexistent
-     * column.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void setValueString_givenNameOfNonexistentColumn_throwsIllegalArgumentException() {
-        record.setValue("null", value1);
-    }
-
-    /**
-     * Tests whether {@link Record#setValue(String, String)} throws an
-     * {@link IllegalStateException} when given a name of a column that contains
-     * {@link Double}s.
+     * Tests whether {@link Record#setValue(String, Object)} throws an
+     * {@link IllegalStateException} when given a {@link String} value and the
+     * name of a column that contains {@link Double}s.
      */
     @Test(expected = IllegalStateException.class)
     public void setValueString_givenNameOfColumnWithNumber_throwsIllegalStateException() {
@@ -259,9 +363,19 @@ public class RecordTest {
     }
 
     /**
-     * Tests whether {@link Record#setValue(String, String)} updates the value
-     * of the correct column when given the name of a column that contains
-     * {@link String}s.
+     * Tests whether {@link Record#setValue(String, Object)} throws an
+     * {@link IllegalStateException} when given a {@link String} value and the
+     * name of a column that contains {@link LocalDate}s.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void setValueString_givenNameOfColumnWithDate_throwsIllegalStateException() {
+        record.setValue(column3.getName(), value1);
+    }
+
+    /**
+     * Tests whether {@link Record#setValue(String, Object)} updates the value
+     * of the correct column when given a {@link String} value and the name of a
+     * column that contains {@link String}s.
      */
     @Test
     public void setValueString_givenNameOfColumn_updatesValue() {
@@ -273,5 +387,77 @@ public class RecordTest {
         String actual = record.getStringValue(column1.getName());
 
         assertSame(expected, actual);
+    }
+
+    /**
+     * Tests whether {@link Record#setValue(String, Object)} throws an
+     * {@link IllegalStateException} when given a {@link LocalDate} value and
+     * the name of a column that contains {@link Double}s.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void setValueDate_givenNameOfColumnWithNumber_throwsIllegalStateException() {
+        record.setValue(column2.getName(), value3);
+    }
+
+    /**
+     * Tests whether {@link Record#setValue(String, Object)} throws an
+     * {@link IllegalStateException} when given a {@link LocalDate} value and
+     * the name of a column that contains {@link String}s.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void setValueDate_givenNameOfColumnWithString_throwsIllegalStateException() {
+        record.setValue(column1.getName(), value3);
+    }
+
+    /**
+     * Tests whether {@link Record#setValue(String, Object)} updates the value
+     * of the correct column when given {@link LocalDate} value and the name of
+     * a column that contains {@link LocalDate}s.
+     */
+    @Test
+    public void setValueDate_givenNameOfColumn_updatesValue() {
+        LocalDate value = LocalDate.of(1971, 1, 1);
+
+        record.setValue(column3.getName(), value);
+
+        LocalDate expected = value;
+        LocalDate actual = record.getDateValue(column3.getName());
+
+        assertSame(expected, actual);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void copyTo_givenTableNull_throwsNullPointerException() {
+        record.copyTo(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void copyTo_givenTableWithDifferentNumberOfColumns_throwsIllegalArgumentException() {
+        Table table2 = mock(Table.class);
+        when(table2.getColumns()).thenReturn(Arrays.asList(column1, column2));
+
+        record.copyTo(table2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void copyTo_givenTableWithDifferentColumns_throwsIllegalArgumentException() {
+        Table table2 = mock(Table.class);
+        when(table2.getColumns()).thenReturn(Arrays.asList(column1, column3, column2));
+
+        record.copyTo(table2);
+    }
+
+    @Test
+    public void copyTo_givenTableWithIdenticalColumns_copiesRecord() {
+        Table table2 = mock(Table.class);
+        when(table2.getColumns()).thenReturn(Arrays.asList(column1, column2, column3));
+
+        record.copyTo(table2);
+
+        ArgumentCaptor<Record> recordCaptur = ArgumentCaptor.forClass(Record.class);
+        verify(table2).addRecord(recordCaptur.capture());
+
+        Record copy = recordCaptur.getValue();
+        assertEquals(record.getValues(), copy.getValues());
     }
 }
