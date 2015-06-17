bcebb391d169ed800502f392e1a69ee995f4944b
adamIqbal <hamier08@gmail.com>
2015-06-17 12:22:45 +0200

    generated test case constraint

---
+++ health/src/test/java/com/health/operations/ConstraintsTest.java
@@ -0,0 +1,19 @@
+package com.health.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConstraintsTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void testConstrain() {
+    fail("Not yet implemented");
+  }
+
+}
