1a74d0a2c10a75b73a34763d92c7a83f4cf64a60
Saffie <bjorn.vdlaan@gmail.com>
2015-06-18 10:23:04 +0200

    fixed javascript erro

--- health/src/main/java/com/health/gui/script/TextLineNumber.java
+++ health/src/main/java/com/health/gui/script/TextLineNumber.java
@@ -176,7 +176,7 @@ public class TextLineNumber extends JPanel
      *  <li>TextLineNumber.CENTER
      *  <li>TextLineNumber.RIGHT (default)
      *  </ul>
-     *  @param currentLineForeground  the Color used to render the current line
+     *  @param digitAlignment the alignment of digits
      */
     public void setDigitAlignment(float digitAlignment)
     {
