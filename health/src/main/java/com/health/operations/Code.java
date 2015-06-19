cbddd4e786340a85052f5179654dd8216f4cef3e
adamIqbal <hamier08@gmail.com>
2015-06-19 10:56:32 +0200

    format works again

--- health/src/main/java/com/health/operations/Code.java
+++ health/src/main/java/com/health/operations/Code.java
@@ -15,152 +15,159 @@ import com.health.Table;
 
 public class Code {
 
-  /**
-   * an unused constructor.
-   */
-  protected Code() {
-  }
+	/**
+	 * an unused constructor.
+	 */
+	protected Code() {
+	}
 
-  /**
-   * Looks for all event declared and puts them in an evenList.
-   * 
-   * @param table
-   *          the table in which to look for events.
-   * @param codes
-   *          the event declaration.
-   * @return an EventList with all found events.
-   */
-  public static EventList makeEventList(final Table table,
-      final Map<String, Function<Record, Boolean>> codes) {
-    EventList list = new EventList();
+	/**
+	 * Looks for all event declared and puts them in an evenList.
+	 * 
+	 * @param table
+	 *            the table in which to look for events.
+	 * @param codes
+	 *            the event declaration.
+	 * @return an EventList with all found events.
+	 */
+	public static EventList makeEventList(final Table table,
+			final Map<String, Function<Record, Boolean>> codes) {
+		EventList list = new EventList();
 
-    if (codes == null) {
-      return list;
-    }
-    for (Record record : table.getRecords()) {
-      for (Entry<String, Function<Record, Boolean>> entry : codes.entrySet()) {
-        if (entry.getValue().apply(record)) {
-          Event tmp = new Event(entry.getKey(), record);
-          list.addEvent(tmp);
-        }
-      }
-    }
+		if (codes == null) {
+			return list;
+		}
+		for (Record record : table.getRecords()) {
+			for (Entry<String, Function<Record, Boolean>> entry : codes
+					.entrySet()) {
+				if (entry.getValue().apply(record)) {
+					Event tmp = new Event(entry.getKey(), record);
+					list.addEvent(tmp);
+				}
+			}
+		}
 
-    list.orderListByDate();
-    return list;
-  }
+		list.orderListByDate();
+		return list;
+	}
 
-  /**
-   * Fills up an EventSequence object with EventLists with the codePatern declared in the
-   * EventSequence Object.
-   * 
-   * @param eventSeq
-   *          the EventSequence to be Filled.
-   * @param eventList
-   *          the EventList Object in which to look for the pattern.
-   */
-  public static List<EventList> fillEventSequence(final EventSequence eventSeq,
-      final EventList eventList) {
-    eventList.orderListByDate();
-    List<Event> eList = eventList.getList();
-    String[] codePattern = eventSeq.getCodePattern();
+	/**
+	 * Fills up an EventSequence object with EventLists with the codePatern
+	 * declared in the EventSequence Object.
+	 * 
+	 * @param eventSeq
+	 *            the EventSequence to be Filled.
+	 * @param eventList
+	 *            the EventList Object in which to look for the pattern.
+	 */
+	public static List<EventList> fillEventSequence(
+			final EventSequence eventSeq, final EventList eventList) {
+		eventList.orderListByDate();
+		List<Event> eList = eventList.getList();
+		String[] codePattern = eventSeq.getCodePattern();
 
-    int currentIndex = 0;
+		int currentIndex = 0;
 
-    while (currentIndex < eList.size()) {
-      EventList tmpEList = new EventList();
+		while (currentIndex < eList.size()) {
+			EventList tmpEList = new EventList();
 
-      makeEventList: if (eventSeq.isConnected()) {
+			makeEventList: if (eventSeq.isConnected()) {
 
-        currentIndex = findEventInSeq(codePattern[0], eventList, currentIndex);
+				currentIndex = findEventInSeq(codePattern[0], eventList,
+						currentIndex);
 
-        // start of pattern not found
-        if (currentIndex == -1) {
-          break;
-        }
+				// start of pattern not found
+				if (currentIndex == -1) {
+					break;
+				}
 
-        tmpEList.addEvent(eList.get(currentIndex));
-        int oldIndex = currentIndex;
-        int nextIndex = ++currentIndex;
-        for (int i = 1; i < codePattern.length; i++) {
-          nextIndex = findEventInSeq(codePattern[i], eventList, nextIndex);
-          if (nextIndex - oldIndex == 1) {
-            tmpEList.addEvent(eList.get(nextIndex));
-            oldIndex = nextIndex;
-            nextIndex++;
-          } else {
-            break makeEventList;
-          }
-        }
+				tmpEList.addEvent(eList.get(currentIndex));
+				int oldIndex = currentIndex;
+				int nextIndex = ++currentIndex;
+				for (int i = 1; i < codePattern.length; i++) {
+					nextIndex = findEventInSeq(codePattern[i], eventList,
+							nextIndex);
+					if (nextIndex - oldIndex == 1) {
+						tmpEList.addEvent(eList.get(nextIndex));
+						oldIndex = nextIndex;
+						nextIndex++;
+					} else {
+						break makeEventList;
+					}
+				}
 
-        eventSeq.addSequence(tmpEList);
-      } else {
-        // start of pattern not found
-        if (currentIndex == -1) {
-          break;
-        }
+				eventSeq.addSequence(tmpEList);
+			} else {
+				// start of pattern not found
+				if (currentIndex == -1) {
+					break;
+				}
 
-        for (String code : codePattern) {
-          currentIndex = findEventInSeq(code, eventList, currentIndex);
-          if (currentIndex == -1) {
-            break makeEventList;
-          }
-          tmpEList.addEvent(eList.get(currentIndex));
-          currentIndex++;
-        }
+				for (String code : codePattern) {
+					currentIndex = findEventInSeq(code, eventList, currentIndex);
+					if (currentIndex == -1) {
+						break makeEventList;
+					}
+					tmpEList.addEvent(eList.get(currentIndex));
+					currentIndex++;
+				}
 
-        eventSeq.addSequence(tmpEList);
-      }
+				eventSeq.addSequence(tmpEList);
+			}
 
-    }
+		}
 
-    return eventSeq.getSequences();
-  }
+		return eventSeq.getSequences();
+	}
 
-  /**
-   * Fills up an EventSequence object with EventLists with the codePatern declared in the
-   * EventSequence Object.
-   * 
-   * @param eventSeq
-   *          the EventSequence to be Filled.
-   * @param eventList
-   *          the EventList Object in which to look for the pattern.
-   * @param per
-   *          the maximum amount of time between the first event of the sequence and the last.
-   */
-  public static void fillEventSequenceWithinPeriod(final EventSequence eventSeq,
-      final EventList eventList, final Period per) {
-    fillEventSequence(eventSeq, eventList);
+	/**
+	 * Fills up an EventSequence object with EventLists with the codePatern
+	 * declared in the EventSequence Object.
+	 * 
+	 * @param eventSeq
+	 *            the EventSequence to be Filled.
+	 * @param eventList
+	 *            the EventList Object in which to look for the pattern.
+	 * @param per
+	 *            the maximum amount of time between the first event of the
+	 *            sequence and the last.
+	 */
+	public static void fillEventSequenceWithinPeriod(
+			final EventSequence eventSeq, final EventList eventList,
+			final Period per) {
+		fillEventSequence(eventSeq, eventList);
 
-    deleteOutSideOfPeriod(eventSeq, per);
-  }
+		deleteOutSideOfPeriod(eventSeq, per);
+	}
 
-  private static int findEventInSeq(final String code, final EventList eventList,
-      final int currentIndex) {
+	private static int findEventInSeq(final String code,
+			final EventList eventList, final int currentIndex) {
 
-    List<Event> eList = eventList.getList();
-    try {
-      for (int i = currentIndex; i < eList.size(); i++) {
-        if (eList.get(i).getCode().equals(code)) {
-          return i;
-        }
-      }
-    } catch (ArrayIndexOutOfBoundsException e) {
-      return -1;
-    }
-    return -1;
-  }
+		List<Event> eList = eventList.getList();
+		try {
+			for (int i = currentIndex; i < eList.size(); i++) {
+				if (eList.get(i).getCode().equals(code)) {
+					return i;
+				}
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return -1;
+		}
+		return -1;
+	}
 
-  private static void deleteOutSideOfPeriod(final EventSequence eventSeq, final Period per) {
-    List<EventList> seqList = eventSeq.getSequences();
-    for (EventList eventList : seqList) {
-      Event firstEvent = eventList.getList().get(0);
-      Event lastEvent = eventList.getList().get(eventList.getList().size() - 1);
+	private static void deleteOutSideOfPeriod(final EventSequence eventSeq,
+			final Period per) {
+		List<EventList> seqList = eventSeq.getSequences();
+		for (EventList eventList : seqList) {
+			Event firstEvent = eventList.getList().get(0);
+			Event lastEvent = eventList.getList().get(
+					eventList.getList().size() - 1);
 
-      LocalDateTime endOfPer = firstEvent.getDate().plus(per);
-      if (lastEvent.getDate().isAfter(endOfPer)) {
-        eventSeq.deleteSequence(eventList);
-      }
-    }
-  }
+			LocalDateTime endOfPer = firstEvent.getDate().plus(per);
+			if (lastEvent.getDate().isAfter(endOfPer)) {
+				eventSeq.deleteSequence(eventList);
+			}
+		}
+	}
 }
--- health/src/main/java/com/health/operations/ReadTime.java
+++ health/src/main/java/com/health/operations/ReadTime.java
@@ -17,43 +17,46 @@ import com.health.ValueType;
  */
 public class ReadTime {
 
-  /**
-   * an unused constructor.
-   */
-  protected ReadTime() {
+	/**
+	 * an unused constructor.
+	 */
+	protected ReadTime() {
 
-  }
+	}
 
-  /**
-   * Adds the time in the timeCol to the date in de dateCol.
-   * 
-   * @param table
-   *          the table to manipulate.
-   * @param dateCol
-   *          the columns which has the date.
-   * @param timeCol
-   *          the collumns which has the time as a number between 0 and 2400.
-   */
-  public static void addTimeToDate(final Table table, final Column dateCol, final Column timeCol) {
-    if (dateCol.getType() != ValueType.Date && timeCol.getType() != ValueType.Number) {
-      return;
-    }
+	/**
+	 * Adds the time in the timeCol to the date in de dateCol.
+	 * 
+	 * @param table
+	 *            the table to manipulate.
+	 * @param dateCol
+	 *            the columns which has the date.
+	 * @param timeCol
+	 *            the collumns which has the time as a number between 0 and
+	 *            2400.
+	 */
+	public static void addTimeToDate(final Table table, final Column dateCol,
+			final Column timeCol) {
+		if (dateCol.getType() != ValueType.Date
+				&& timeCol.getType() != ValueType.Number) {
+			return;
+		}
 
-    List<Record> recordList = table.getRecords();
-    for (Record rec : recordList) {
+		List<Record> recordList = table.getRecords();
+		for (Record rec : recordList) {
 
-      LocalDateTime date = rec.getDateValue(dateCol.getName());
+			LocalDateTime date = rec.getDateValue(dateCol.getName());
 
-      double time = rec.getNumberValue(timeCol.getName());
+			double time = rec.getNumberValue(timeCol.getName());
 
-      long min = (long) (time % 100);
-      date = date.plus(min, ChronoUnit.MINUTES);
+			long min = (long) (time % 100);
+			date = date.plus(min, ChronoUnit.MINUTES);
 
-      long hours = (long) ((time - min) / 100);
-      date = date.plus(hours, ChronoUnit.HOURS);
+			long hours = (long) ((time - min) / 100);
+			date = date.plus(hours, ChronoUnit.HOURS);
 
-      rec.setValue(table.getDateColumn().getName(), date);
+			rec.setValue(table.getDateColumn().getName(), date);
 
-    }
-  }
+		}
+	}
 }
