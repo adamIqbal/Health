50f8ed697c6f3dc17d731ebdfce76a011073bce5
LizzyScholten <lizzyxmissxsportyx@hotmail.com>
2015-06-19 10:18:24 +0200

    auto formatted and checkstyled. some method restructuring

--- health/src/main/java/com/health/visuals/FreqBar.java
+++ health/src/main/java/com/health/visuals/FreqBar.java
@@ -32,215 +32,217 @@ import de.erichseifert.vectorgraphics2d.VectorGraphics2D;
  *
  */
 public final class FreqBar {
-    /**
-     * Private constructor to prevent instantiation.
-     */
-    private FreqBar() {
-        // Nothing happens
-    }
+	/**
+	 * Private constructor to prevent instantiation.
+	 */
+	private FreqBar() {
+		// Nothing happens
+	}
 
-    /**
-     * Generates a Frequency bar diagram. This variant has no column specified.
-     * It chooses the last date column and last frequency column in the Table
-     * object.
-     *
-     * @param table
-     *            Table to use
-     * @return
-     * 			Chart
-     */
-    public static Chart frequencyBar(final Table table) {
-        // Check if the Table contains a frequency and a date column
-        Column freqColumn = null;
-        Column dateColumn = null;
-        for (Column c : table.getColumns()) {
-            if (c.isFrequencyColumn()) {
-                freqColumn = c;
-            } else if (c.getType() == ValueType.Date) {
-                dateColumn = c;
-            }
-        }
-        // If both exist, format the frequency map based on these columns.
-        if (freqColumn != null && dateColumn != null) {
-            Map<String, Integer> freqMap = formatFrequencyMap(table,
-                    freqColumn.getName(), dateColumn.getName());
-            return makeBarChart(freqMap, dateColumn.getName());
-        } else {
-            // Not good.
-            throw new RuntimeException("Table contains either no frequency column or no date column.");
-        }
-    }
+	/**
+	 * Generates a Frequency bar diagram. This variant has no column specified.
+	 * It chooses the last date column and last frequency column in the Table
+	 * object.
+	 *
+	 * @param table
+	 *            Table to use
+	 * @return Chart
+	 */
+	public static Chart frequencyBar(final Table table) {
+		// Check if the Table contains a frequency and a date column
+		Column freqColumn = null;
+		Column dateColumn = null;
+		for (Column c : table.getColumns()) {
+			if (c.isFrequencyColumn()) {
+				freqColumn = c;
+			} else if (c.getType() == ValueType.Date) {
+				dateColumn = c;
+			}
+		}
+		// If both exist, format the frequency map based on these columns.
+		if (freqColumn != null && dateColumn != null) {
+			Map<String, Integer> freqMap = formatFrequencyMap(table,
+					freqColumn.getName(), dateColumn.getName());
+			return makeBarChart(freqMap, dateColumn.getName());
+		} else {
+			// Not good.
+			throw new RuntimeException(
+					"Table contains either no frequency column or no date column.");
+		}
+	}
 
-    /**
-     * Generates a Frequency Bar diagram.
-     *
-     * @param table
-     *            Table to use
-     * @param column
-     *            Column to display frequency of
-     * @return
-     * 			Chart
-     */
-    public static Chart frequencyBar(final Table table, final String column) {
-        // Check if the Table contains a frequency column
-        Column freqColumn = null;
-        for (Column c : table.getColumns()) {
-            if (c.isFrequencyColumn()) {
-                freqColumn = c;
-            }
-        }
-        // If the Table contains a frequency column, use it to format the
-        // frequency map
-        // Else if no frequency column exists, count occurrences of values in
-        // the specified column
-        if (freqColumn != null) {
-            Map<String, Integer> freqMap = formatFrequencyMap(table, freqColumn.getName(), column);
-            return makeBarChart(freqMap, column);
-        } else {
-            Map<String, Integer> freqMap = createFrequencyMap(table, column);
-            return makeBarChart(freqMap, column);
-        }
-    }
+	/**
+	 * Generates a Frequency Bar diagram.
+	 *
+	 * @param table
+	 *            Table to use
+	 * @param column
+	 *            Column to display frequency of
+	 * @return Chart
+	 */
+	public static Chart frequencyBar(final Table table, final String column) {
+		// Check if the Table contains a frequency column
+		Column freqColumn = null;
+		for (Column c : table.getColumns()) {
+			if (c.isFrequencyColumn()) {
+				freqColumn = c;
+			}
+		}
+		// If the Table contains a frequency column, use it to format the
+		// frequency map
+		// Else if no frequency column exists, count occurrences of values in
+		// the specified column
+		if (freqColumn != null) {
+			Map<String, Integer> freqMap = formatFrequencyMap(table,
+					freqColumn.getName(), column);
+			return makeBarChart(freqMap, column);
+		} else {
+			Map<String, Integer> freqMap = createFrequencyMap(table, column);
+			return makeBarChart(freqMap, column);
+		}
+	}
 
-    /**
-     * Creates a frequency map from the input Table to serve as input for
-     * makeBarChart.
-     *
-     * @param table
-     *            Table to use
-     * @param freqColumn
-     *            the frequency column
-     * @param column
-     *            the column
-     * @return frequency map
-     */
-    private static Map<String, Integer> formatFrequencyMap(final Table table,
-            final String freqColumn, final String column) {
-        // Create map to save frequencies
-        Map<String, Integer> freqMap = new HashMap<String, Integer>();
+	/**
+	 * Creates a frequency map from the input Table to serve as input for
+	 * makeBarChart.
+	 *
+	 * @param table
+	 *            Table to use
+	 * @param freqColumn
+	 *            the frequency column
+	 * @param column
+	 *            the column
+	 * @return frequency map
+	 */
+	private static Map<String, Integer> formatFrequencyMap(final Table table,
+			final String freqColumn, final String column) {
+		// Create map to save frequencies
+		Map<String, Integer> freqMap = new HashMap<String, Integer>();
 
-        for (Record r : table) {
-            String value = r.getValue(column).toString();
-            double frequency = (Double) r.getValue(freqColumn);
-            freqMap.put(value, (int) frequency);
-        }
+		for (Record r : table) {
+			String value = r.getValue(column).toString();
+			double frequency = (Double) r.getValue(freqColumn);
+			freqMap.put(value, (int) frequency);
+		}
 
-        return freqMap;
-    }
+		return freqMap;
+	}
 
-    /**
-     * Counts the occurrences of each value of column and creates a frequency
-     * map. Used when no the table contains no frequency column
-     *
-     * @param table
-     *            Table to use
-     * @param column
-     *            Column to count
-     * @return frequency map
-     */
-    private static Map<String, Integer> createFrequencyMap(final Table table,
-            final String column) {
-        // Create map to save frequencies
-        Map<String, Integer> freqMap = new HashMap<String, Integer>();
+	/**
+	 * Counts the occurrences of each value of column and creates a frequency
+	 * map. Used when no the table contains no frequency column
+	 *
+	 * @param table
+	 *            Table to use
+	 * @param column
+	 *            Column to count
+	 * @return frequency map
+	 */
+	private static Map<String, Integer> createFrequencyMap(final Table table,
+			final String column) {
+		// Create map to save frequencies
+		Map<String, Integer> freqMap = new HashMap<String, Integer>();
 
-        for (Record r : table) {
-            // Get value of record
-            String value = r.getValue(column).toString();
-            if (!freqMap.containsKey(value)) {
-                freqMap.put(value, 1);
-            } else {
-                int currentFrequency = freqMap.get(value);
-                freqMap.replace(value, ++currentFrequency);
-            }
-        }
+		for (Record r : table) {
+			// Get value of record
+			String value = r.getValue(column).toString();
+			if (!freqMap.containsKey(value)) {
+				freqMap.put(value, 1);
+			} else {
+				int currentFrequency = freqMap.get(value);
+				freqMap.replace(value, ++currentFrequency);
+			}
+		}
 
-        return freqMap;
-    }
+		return freqMap;
+	}
 
-    /**
-     * Creates a frequency bar diagram based on the frequency map.
-     *
-     * @param freqMap
-     *            frequency map
-     * @param seriesName
-     * 				name of the series
-     * @return
-     * 			chart
-     */
-    private static Chart makeBarChart(final Map<String, Integer> freqMap,
-            final String seriesName) {
-        final int frameWidth = 800;
-        final int frameHeight = 600;
-        // Convert input data for processing
-        ArrayList<String> labels = new ArrayList<String>(freqMap.keySet());
-        ArrayList<Integer> frequency = new ArrayList<Integer>(freqMap.values());
+	/**
+	 * Creates a frequency bar diagram based on the frequency map.
+	 *
+	 * @param freqMap
+	 *            frequency map
+	 * @param seriesName
+	 *            name of the series
+	 * @return chart
+	 */
+	private static Chart makeBarChart(final Map<String, Integer> freqMap,
+			final String seriesName) {
+		final int frameWidth = 800;
+		final int frameHeight = 600;
+		// Convert input data for processing
+		ArrayList<String> labels = new ArrayList<String>(freqMap.keySet());
+		ArrayList<Integer> frequency = new ArrayList<Integer>(freqMap.values());
 
-        // Create Chart
-        Chart chart = new ChartBuilder().chartType(ChartType.Bar)
-                .width(frameWidth).height(frameHeight).title("Score Histogram")
-                .xAxisTitle(seriesName).yAxisTitle("Frequency").build();
+		// Create Chart
+		Chart chart = new ChartBuilder().chartType(ChartType.Bar)
+				.width(frameWidth).height(frameHeight).title("Score Histogram")
+				.xAxisTitle(seriesName).yAxisTitle("Frequency").build();
 
-        chart.addSeries(seriesName, new ArrayList<String>(labels),
-                new ArrayList<Integer>(frequency));
+		chart.addSeries(seriesName, new ArrayList<String>(labels),
+				new ArrayList<Integer>(frequency));
 
-        // Customize Chart
-        chart.getStyleManager().setLegendPosition(LegendPosition.InsideNW);
+		// Customize Chart
+		chart.getStyleManager().setLegendPosition(LegendPosition.InsideNW);
 
-        return chart;
-    }
+		return chart;
+	}
 
-    /**
-     * Container for chart.
-     * @param chart
-     * 			chart
-     * @return
-     * 			Container
-     */
-    public static Container getContainer(final Chart chart) {
-        // Wrap the chart in a JFrame and hide the frame
-        JFrame frame = new SwingWrapper(chart).displayChart();
-        frame.addWindowListener(new HideWindowAdapter());
+	/**
+	 * Container for chart.
+	 *
+	 * @param chart
+	 *            chart
+	 * @return Container
+	 */
+	public static Container getContainer(final Chart chart) {
+		// Wrap the chart in a JFrame and hide the frame
+		JFrame frame = new SwingWrapper(chart).displayChart();
+		frame.addWindowListener(new HideWindowAdapter());
 
-        return frame.getContentPane();
-    }
+		return frame.getContentPane();
+	}
 
-    /**
-     * Save the chart as pdf.
-     * @param chart
-     * 			chart that should be saved.
-     * @param fileName
-     * 			file name under which the chart should be saved.
-     * @throws IOException
-     * 			i/o exception
-     */
-    public static void saveGraph(final Chart chart, final String fileName) throws IOException {
-    	final int width = (int) PageSize.A4.getWidth();
-        final int height = (int) PageSize.A4.getHeight();
+	/**
+	 * Save the chart as pdf.
+	 *
+	 * @param chart
+	 *            chart that should be saved.
+	 * @param fileName
+	 *            file name under which the chart should be saved.
+	 * @throws IOException
+	 *             i/o exception
+	 */
+	public static void saveGraph(final Chart chart, final String fileName)
+			throws IOException {
+		final int width = (int) PageSize.A4.getWidth();
+		final int height = (int) PageSize.A4.getHeight();
 
-        VectorGraphics2D g = new PDFGraphics2D(0.0, 0.0, width, height);
+		VectorGraphics2D g = new PDFGraphics2D(0.0, 0.0, width, height);
 
-        chart.paint(g, width, height);
+		chart.paint(g, width, height);
 
-        // Write the vector graphic output to a file
-        FileOutputStream file = new FileOutputStream(fileName + ".pdf");
+		// Write the vector graphic output to a file
+		FileOutputStream file = new FileOutputStream(fileName + ".pdf");
 
-        try {
-            file.write(g.getBytes());
-        } finally {
-            file.close();
-        }
-    }
+		try {
+			file.write(g.getBytes());
+		} finally {
+			file.close();
+		}
+	}
 
-    /**
-     * Hides window.
-     * @author lizzy
-     *
-     */
-    private static class HideWindowAdapter extends WindowAdapter {
-        @Override
-        public void windowActivated(final WindowEvent e) {
-            e.getWindow().setVisible(false);
+	/**
+	 * Hides window.
+	 *
+	 * @author Martijn
+	 *
+	 */
+	private static class HideWindowAdapter extends WindowAdapter {
+		@Override
+		public void windowActivated(final WindowEvent e) {
+			e.getWindow().setVisible(false);
 
-        }
-    }
+		}
+	}
 }
--- health/src/main/java/com/health/visuals/StateTransitionMatrix.java
+++ health/src/main/java/com/health/visuals/StateTransitionMatrix.java
@@ -21,11 +21,10 @@ import org.jfree.ui.ApplicationFrame;
 import com.health.Event;
 import com.health.EventList;
 import com.health.EventSequence;
+import com.health.operations.Code;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.health.operations.Code;
-
 
 /**
  * Creates a State Transition Matrix.
@@ -35,235 +34,235 @@ import com.health.operations.Code;
  */
 public final class StateTransitionMatrix extends JFrame {
 
-    /**
-     * Serial ID.
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * Serial ID.
+	 */
+	private static final long serialVersionUID = 1L;
 
-    private StateTransitionMatrix() {
-        // Does nothing
-    }
+	private StateTransitionMatrix() {
+		// Does nothing
+	}
 
-    /**
-     * Create a state transition matrix.
-     * @param eList
-     * 			EventList
-     * @return
-     * 			JTable
-     */
-    public static Container createStateTrans(final EventList eList) {
-        List<EventList> seqList = findEventSequences(eList);
+	/**
+	 * Create a state transition matrix.
+	 *
+	 * @param eList
+	 *            EventList
+	 * @return JTable
+	 */
+	public static Container createStateTrans(final EventList eList) {
+		List<EventList> seqList = findEventSequences(eList);
 
-        return createStateTrans(eList, seqList);
-    }
+		return createStateTrans(eList, seqList);
+	}
 
-    private static List<EventList> findEventSequences(final EventList eList) {
-        String[] codes = getCodes(eList);
+	private static List<EventList> findEventSequences(final EventList eList) {
+		String[] codes = getCodes(eList);
 
-        List<EventList> seqList = new ArrayList<EventList>();
+		List<EventList> seqList = new ArrayList<EventList>();
 
-        for (int i = 0; i < codes.length; i++) {
-            for (int j = 0; j < codes.length; j++) {
-                EventSequence sequence = new EventSequence(new String[] {
-                        codes[i],
-                        codes[j]
-                }, true);
+		for (int i = 0; i < codes.length; i++) {
+			for (int j = 0; j < codes.length; j++) {
+				EventSequence sequence = new EventSequence(new String[] {
+						codes[i], codes[j] }, true);
 
-                Code.fillEventSequence(sequence, eList);
+				Code.fillEventSequence(sequence, eList);
 
-                seqList.addAll(sequence.getSequences());
-            }
-        }
+				seqList.addAll(sequence.getSequences());
+			}
+		}
 
-        return seqList;
-    }
+		return seqList;
+	}
 
-    /**
-     * Get event codes.
-     * @param eList
-     * 			EventList
-     * @return
-     * 		string array
-     */
-    private static String[] getCodes(final EventList eList) {
-        Set<String> codeSet = new HashSet<String>();
+	/**
+	 * Get event codes.
+	 *
+	 * @param eList
+	 *            EventList
+	 * @return string array
+	 */
+	private static String[] getCodes(final EventList eList) {
+		Set<String> codeSet = new HashSet<String>();
 
-        for (Event event : eList.getList()) {
-            codeSet.add(event.getCode());
-        }
+		for (Event event : eList.getList()) {
+			codeSet.add(event.getCode());
+		}
 
-        String[] codes = new String[codeSet.size()];
-        codeSet.toArray(codes);
+		String[] codes = new String[codeSet.size()];
+		codeSet.toArray(codes);
 
-        return codes;
-    }
+		return codes;
+	}
 
-    /**
-     * Create State Transition Matrix.
-     *
-     * @param eList
-     *            list with possible events
-     * @param seqList
-     *            list with sequences
-     * @return
-     * 			JTable
-     */
-    public static Container createStateTrans(final EventList eList, final List<EventList> seqList) {
-        // Create frame
-    	final Dimension frameDimension = new Dimension((int) (PageSize.A4.getWidth()), (int) (PageSize.A4.getHeight()));
+	/**
+	 * Create State Transition Matrix.
+	 *
+	 * @param eList
+	 *            list with possible events
+	 * @param seqList
+	 *            list with sequences
+	 * @return JTable
+	 */
+	public static Container createStateTrans(final EventList eList,
+			final List<EventList> seqList) {
+		// Create frame
+		final Dimension frameDimension = new Dimension(
+				(int) (PageSize.A4.getWidth()), (int) (PageSize.A4.getHeight()));
 
-        ApplicationFrame frame = new ApplicationFrame("Vidney");
-        frame.setSize(frameDimension);
+		ApplicationFrame frame = new ApplicationFrame("Vidney");
+		frame.setSize(frameDimension);
 
-        String[][] matrix = setUp(eList);
-        String[][] matrixUse = fillMatrix(matrix, seqList);
-        String[][] outputM = new String[matrixUse.length - 1][matrixUse.length];
+		String[][] matrix = setUp(eList);
+		String[][] matrixUse = fillMatrix(matrix, seqList);
+		String[][] outputM = new String[matrixUse.length - 1][matrixUse.length];
 
-        for (int i = 1; i < matrixUse[0].length; i++) {
-            for (int j = 0; j < matrixUse[1].length; j++) {
-                outputM[i - 1][j] = matrixUse[i][j];
-            }
-        }
+		for (int i = 1; i < matrixUse[0].length; i++) {
+			for (int j = 0; j < matrixUse[1].length; j++) {
+				outputM[i - 1][j] = matrixUse[i][j];
+			}
+		}
 
-        JTable table = new JTable(outputM, matrixUse[0]);
+		JTable table = new JTable(outputM, matrixUse[0]);
 
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setColumnHeaderView(table.getTableHeader());
-        scrollPane.setPreferredSize(table.getPreferredSize());
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setColumnHeaderView(table.getTableHeader());
+		scrollPane.setPreferredSize(table.getPreferredSize());
 
-        JPanel p = new JPanel(new BorderLayout());
-        p.add(scrollPane, BorderLayout.CENTER);
+		JPanel p = new JPanel(new BorderLayout());
+		p.add(scrollPane, BorderLayout.CENTER);
 
 		// Not a very clean way
-        frame.setContentPane(p);
-        frame.setVisible(true);
-        frame.setVisible(false);
+		frame.setContentPane(p);
+		frame.setVisible(true);
+		frame.setVisible(false);
 
-        return scrollPane;
-    }
+		return scrollPane;
+	}
 
+	/**
+	 * Set up matrix.
+	 *
+	 * @param eList
+	 *            event list
+	 * @return matrix setup
+	 */
+	private static String[][] setUp(final EventList eList) {
 
-    /**
-     * Set up matrix.
-     *
-     * @param eList
-     *            event list
-     * @return matrix setup
-     */
-    private static String[][] setUp(final EventList eList) {
+		ArrayList<String> eArr = new ArrayList<String>();
 
-        ArrayList<String> eArr = new ArrayList<String>();
+		for (Event e : eList.getList()) {
+			if (!eArr.contains(e.getCode())) {
+				eArr.add(e.getCode());
+			}
+		}
 
-        for (Event e : eList.getList()) {
-            if (!eArr.contains(e.getCode())) {
-                eArr.add(e.getCode());
-            }
-        }
+		String[][] matrix = new String[eArr.size() + 1][eArr.size() + 1];
 
-        String[][] matrix = new String[eArr.size() + 1][eArr.size() + 1];
+		matrix[0][0] = "Event types";
 
-        matrix[0][0] = "Event types";
+		for (int k = 1; k < eArr.size() + 1; k++) {
+			matrix[0][k] = eArr.get(k - 1);
+			matrix[k][0] = eArr.get(k - 1);
+		}
 
-        for (int k = 1; k < eArr.size() + 1; k++) {
-            matrix[0][k] = eArr.get(k - 1);
-            matrix[k][0] = eArr.get(k - 1);
-        }
+		return matrix;
+	}
 
-        return matrix;
-    }
+	/**
+	 * Fill matrix.
+	 *
+	 * @param m
+	 *            matrix
+	 * @param seqList
+	 *            list of sequences
+	 * @return matrix
+	 */
+	private static String[][] fillMatrix(final String[][] m,
+			final List<EventList> seqList) {
+		String[][] matrix = m;
+		for (EventList eSeq : seqList) {
+			String[] codePat = getCodePattern(eSeq);
 
-    /**
-     * Fill matrix.
-     *
-     * @param m
-     *            matrix
-     * @param seqList
-     *            list of sequences
-     * @return matrix
-     */
-    private static String[][] fillMatrix(final String[][] m, final List<EventList> seqList) {
-        String[][] matrix = m;
-        for (EventList eSeq : seqList) {
-            String[] codePat = getCodePattern(eSeq);
+			for (int c = 1; c < codePat.length; c++) {
+				String from = codePat[c - 1];
+				String to = codePat[c];
 
-            for (int c = 1; c < codePat.length; c++) {
-                String from = codePat[c - 1];
-                String to = codePat[c];
+				for (int a = 1; a < matrix[0].length; a++) {
+					boolean found1 = false;
 
-                for (int a = 1; a < matrix[0].length; a++) {
-                    boolean found1 = false;
+					if (matrix[a][0].equals(from)) {
+						for (int b = 1; b < matrix[1].length; b++) {
+							if (matrix[0][b].equals(to)) {
+								if (matrix[a][b] == null) {
+									matrix[a][b] = "1";
+								} else {
+									int val = Integer.parseInt(matrix[a][b]);
+									val = val + 1;
+									matrix[a][b] = Integer.toString(val);
+								}
+								found1 = true;
+								break;
+							}
+						}
+					}
+					if (found1) {
+						break;
+					}
+				}
+			}
+		}
 
-                    if (matrix[a][0].equals(from)) {
-                        for (int b = 1; b < matrix[1].length; b++) {
-                            if (matrix[0][b].equals(to)) {
-                                if (matrix[a][b] == null) {
-                                    matrix[a][b] = "1";
-                                } else {
-                                    int val = Integer.parseInt(matrix[a][b]);
-                                    val = val + 1;
-                                    matrix[a][b] = Integer.toString(val);
-                                }
-                                found1 = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (found1) {
-                        break;
-                    }
-                }
-            }
-        }
+		return matrix;
+	}
 
-        return matrix;
-    }
+	private static String[] getCodePattern(final EventList eSeq) {
+		List<Event> events = eSeq.getList();
+		String[] codes = new String[events.size()];
 
-    private static String[] getCodePattern(final EventList eSeq) {
-        List<Event> events = eSeq.getList();
-        String[] codes = new String[events.size()];
+		for (int i = 0; i < codes.length; i++) {
+			codes[i] = events.get(i).getCode();
+		}
 
-        for (int i = 0; i < codes.length; i++) {
-            codes[i] = events.get(i).getCode();
-        }
+		return codes;
+	}
 
-        return codes;
-    }
-
-
-    /**
-     * Saves matrix to file.
-     * @param name
-     * 			name under which the file should be saved
-     * @param table
-     * 			table you want to save
-     *
-     */
+	/**
+	 * Saves matrix to file.
+	 *
+	 * @param name
+	 *            name under which the file should be saved
+	 * @param table
+	 *            table you want to save
+	 *
+	 */
 	public static void saveFile(final String name, final Container table) {
-		com.itextpdf.text.Document document = new com.itextpdf.text.Document(PageSize.A4
-				);
+		com.itextpdf.text.Document document = new com.itextpdf.text.Document(
+				PageSize.A4);
 		try {
-            int width = table.getWidth();
-            int height = table.getHeight();
+			int width = table.getWidth();
+			int height = table.getHeight();
 
-		  	PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(name + ".pdf"));
-		  	document.open();
+			PdfWriter writer = PdfWriter.getInstance(document,
+					new FileOutputStream(name + ".pdf"));
+			document.open();
 
-		    PdfContentByte cb = writer.getDirectContent();
+			PdfContentByte cb = writer.getDirectContent();
 
-		    cb.saveState();
-		    Graphics2D g2 = cb.createGraphicsShapes(width, height);
+			cb.saveState();
+			Graphics2D g2 = cb.createGraphicsShapes(width, height);
 
-		    Shape oldClip = g2.getClip();
-		    g2.clipRect(0, 0, width, height);
+			Shape oldClip = g2.getClip();
+			g2.clipRect(0, 0, width, height);
 
-		    table.print(g2);
-		    g2.setClip(oldClip);
+			table.print(g2);
+			g2.setClip(oldClip);
 
-		    g2.dispose();
-		    cb.restoreState();
-		  } catch (Exception e) {
-			  System.err.println(e.getMessage());
-		  }
+			g2.dispose();
+			cb.restoreState();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 		document.close();
 	}
 }
