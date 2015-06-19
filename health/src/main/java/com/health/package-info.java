ea1e6253c99b5ca6af729c75ad32973f6a2a4e51
daanvermunt <daanvermunt@gmail.com>
2015-06-19 11:29:58 +0200

    added package info to com health

--- health/src/main/java/com/health/package-info.java
+++ health/src/main/java/com/health/package-info.java
@@ -1,4 +1,6 @@
 /**
- * Package documentation goes here.
+ * The main package of the project, it contains the main function and all datatypes we use in the program.
+ *
+ * @author Martijn , daan
  */
 package com.health;
