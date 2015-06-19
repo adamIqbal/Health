39589fff57c1878daccf5f64063b734654f7e0e8
adamIqbal <hamier08@gmail.com>
2015-06-19 09:17:03 +0200

    format constraint

--- health/src/main/java/com/health/operations/Constraints.java
+++ health/src/main/java/com/health/operations/Constraints.java
@@ -9,30 +9,29 @@ import com.health.Table;
  * A utility class to perform constraining of tables.
  */
 public final class Constraints {
-    private Constraints() {
+  private Constraints() {
+  }
+
+  /**
+   * Creates a new table containing only the records from the given table for which the given
+   * predicate is evaluated as true.
+   *
+   * @param predicate
+   *          a predicate that determines whether or not to add a given record to the new table.
+   * @param table
+   *          the table to constrain on.
+   * @return a new table containing only the records for which the given predicate is evaluated as
+   *         true.
+   */
+  public static Table constrain(final Function<Record, Boolean> predicate, final Table table) {
+    Table constrained = new Table(table.getColumns());
+
+    for (Record record : table.getRecords()) {
+      if (predicate.apply(record)) {
+        record.copyTo(constrained);
+      }
     }
 
-    /**
-     * Creates a new table containing only the records from the given table for
-     * which the given predicate is evaluated as true.
-     *
-     * @param predicate
-     *            a predicate that determines whether or not to add a given
-     *            record to the new table.
-     * @param table
-     *            the table to constrain on.
-     * @return a new table containing only the records for which the given
-     *         predicate is evaluated as true.
-     */
-    public static Table constrain(final Function<Record, Boolean> predicate, final Table table) {
-        Table constrained = new Table(table.getColumns());
-
-        for (Record record : table.getRecords()) {
-            if (predicate.apply(record)) {
-                record.copyTo(constrained);
-            }
-        }
-
-        return constrained;
-    }
+    return constrained;
+  }
 }
